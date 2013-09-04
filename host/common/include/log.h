9c6892c1305ee9920f525aaa95c6d98e4b115f7a
Jon Szymaniak <jon.szymaniak@gmail.com>
2013-09-04 01:33:41 -0400

    host: Disabled file and line information from log messages by default.
    This information (which is helpful when asking questions on IRC or
    in the forums), can be enabled via the CMake argument,
         -DENABLE_LOG_FILE_INFO=ON

--- host/common/include/log.h
+++ host/common/include/log.h
@@ -14,38 +14,40 @@
 
 /** Logs a verbose message. Does not include function/line information */
 #define bladerf_log_verbose(...) \
-    BLADERF_LOG(BLADERF_LOG_LEVEL_VERBOSE, "VERBOSE", __VA_ARGS__)
+    BLADERF_LOG(BLADERF_LOG_LEVEL_VERBOSE, "[VERBOSE:", __VA_ARGS__)
 
 /** Logs a debug message. Does not include function/line information */
 #define bladerf_log_debug(...) \
-    BLADERF_LOG(BLADERF_LOG_LEVEL_DEBUG, "DEBUG", __VA_ARGS__)
+    BLADERF_LOG(BLADERF_LOG_LEVEL_DEBUG, "[DEBUG:", __VA_ARGS__)
 
 /** Logs an info message. Does not include function/line information*/
 #define bladerf_log_info(...) \
-    BLADERF_LOG(BLADERF_LOG_LEVEL_INFO, "INFO", __VA_ARGS__)
+    BLADERF_LOG(BLADERF_LOG_LEVEL_INFO, "[INFO", __VA_ARGS__)
 
 /** Logs a warning message. Includes function/line information */
 #define bladerf_log_warning(...) \
-    BLADERF_LOG_LOCATION(BLADERF_LOG_LEVEL_WARNING, "WARNING", __VA_ARGS__)
+    BLADERF_LOG(BLADERF_LOG_LEVEL_WARNING, "[WARNING", __VA_ARGS__)
 
 /** Logs an error message. Includes function/line information */
 #define bladerf_log_error(...) \
-    BLADERF_LOG_LOCATION(BLADERF_LOG_LEVEL_ERROR, "ERROR", __VA_ARGS__)
+    BLADERF_LOG(BLADERF_LOG_LEVEL_ERROR, "[ERROR", __VA_ARGS__)
 
 /** Logs a critical error message. Includes function/line information */
 #define bladerf_log_critical(...) \
-    BLADERF_LOG_LOCATION(BLADERF_LOG_LEVEL_CRITICAL, "CRITICAL", __VA_ARGS__)
+    BLADERF_LOG(BLADERF_LOG_LEVEL_CRITICAL, "[CRITICAL", __VA_ARGS__)
 
 /** @} */
 
-
-#define BLADERF_LOG_LOCATION(LEVEL, LEVEL_STRING, ...)                      \
-    BLADERF_LOG(LEVEL, LEVEL_STRING,                                        \
-            "[" __FILE__ ":" _LOG_STRINGIFY_(__LINE__) "] " __VA_ARGS__)
-
-#define BLADERF_LOG(LEVEL, LEVEL_STRING, ...)                               \
-    do { bladerf_log(LEVEL, LEVEL_STRING " - " __VA_ARGS__); } while (0);
-
+#ifdef LOG_INCLUDE_FILE_INFO
+#   define BLADERF_LOG(LEVEL, LEVEL_STRING, ...) \
+    do { bladerf_log(LEVEL, LEVEL_STRING  \
+                     " @ "  __FILE__ ":" _LOG_STRINGIFY_(__LINE__) "] " \
+                     __VA_ARGS__); \
+    } while (0);
+#else
+#   define BLADERF_LOG(LEVEL, LEVEL_STRING, ...) \
+    do { bladerf_log(LEVEL, LEVEL_STRING "] " __VA_ARGS__); } while (0);
+#endif
 
 /**
  * Writes a message to the logger with the specified log level. This function
