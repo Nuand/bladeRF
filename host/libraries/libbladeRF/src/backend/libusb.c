Nuand/bladeRF

commit 9015bdbb5ca2336a94f41a8575c67e7862fa8073

libbladeRF, bladeRF-flash: Added device identifier info matching to API routines
Initializing, extracting, and comparing device identifier information
(struct bladerf_devinfo) is now exposed through the API. Among other
things, this allow users to check if a device specifier string matches a
device they already have a handle for.

To support this, a copy of bladerf_devinfo information is now kept around in
the device handle; backends are responsible for populating this in their
open() implementation. A couple of the fields were already present, so
this only adds a couple small variables worth of overhead.

Some re-arranging of the linux and libusb probe and open calls has been done
to achieve this. A few warnings and bugs were addressed in this process:

  - The Linux backend was attempting to fill in a serial number via
    a cached entry.

  - There was some potential for a read of an uninitialized portion
    of an otp buffer.

  - Missing serial # has been made non-fatal. (Some developers'
    device do not have serial numbers.)

Files associated with this functionality was moved from host/common
back into the library, which should now be used instead. bladeRF-flash
has been updated to use these new API functions.

host/libraries/libbladeRF/src/backend/libusb.c
@@ -280,7 +280,7 @@ static int change_setting(struct bladerf *dev, uint8_t setting)
 static int lusb_open(struct bladerf **device, struct bladerf_devinfo *info)
 {
     int status, i, n, inf, val;
-	int fx3_status;
+    int fx3_status;
     ssize_t count;
     struct bladerf *dev = NULL;
     struct bladerf_lusb *lusb = NULL;
@@ -341,8 +341,9 @@ static int lusb_open(struct bladerf **device, struct bladerf_devinfo *info)
                 /* Assign libusb function table, backend type and backend */
                 dev->fn = &bladerf_lusb_fn;
                 dev->backend = (void *)lusb;
-                dev->backend_type = BLADERF_BACKEND_LIBUSB;
                 dev->legacy = 0;
+
+                memcpy(&dev->ident, &info, sizeof(struct bladerf_devinfo));
 
                 /* Populate the backend information */
                 lusb->context = context;

host/libraries/libbladeRF/CMakeLists.txt
@@ -2,8 +2,6 @@
 #
 # This file is part of the bladeRF project
 #
-# TODO license text here (see top-level COPYING for time being)
-#
 # Portions of this file have been borrowed from and/or inspired by
 # the CMakeLists.txt from the rtl-sdr project (GPLv2)
 #   http://sdr.osmocom.org/trac/wiki/rtl-sdr
@@ -16,7 +14,7 @@ project(libbladeRF C)
 ################################################################################
 
 set(VERSION_INFO_MAJOR  0)
-set(VERSION_INFO_MINOR  3)
+set(VERSION_INFO_MINOR  4)
 set(VERSION_INFO_PATCH  0)
 set(VERSION_INFO_EXTRA "git")
 include(Version)
@@ -37,9 +35,7 @@ configure_file(
 ##############################
 # Backend Support
 ##############################
-
-option(ENABLE_BACKEND_LIBUSB
-    "Enable libusb backend support"
+option(ENABLE_BACKEND_LIBUSB "Enable libusb backend support"
     ON
 )
 
@@ -126,13 +122,11 @@ set(LIBBLADERF_SOURCE
         src/lms.c
         src/si5338.c
         src/version.h
-        ${BLADERF_HOST_COMMON_SOURCE_DIR}/device_identifier.c
-        ${BLADERF_HOST_COMMON_SOURCE_DIR}/bladerf_devinfo.c
+        src/device_identifier.c
+        src/devinfo.c
         ${BLADERF_HOST_COMMON_SOURCE_DIR}/conversions.c
         ${BLADERF_HOST_COMMON_SOURCE_DIR}/log.c
 )
-
-message(STATUS "In libraries ${BLADERF_HOST_COMMON_SOURCE_DIR}")
 
 if(LIBUSB_FOUND AND ENABLE_BACKEND_LIBUSB)
     set(LIBBLADERF_SOURCE ${LIBBLADERF_SOURCE} src/backend/libusb.c)
@@ -169,11 +163,6 @@ if(NOT DEFINED LIB_INSTALL_DIR)
 endif()
 
 ################################################################################
-# Install udev rules
-################################################################################
-# TODO
-
-################################################################################
 # Generate pkg-config file
 ################################################################################
 add_subdirectory(include)
@@ -184,10 +173,6 @@ endforeach()
 foreach(lib ${LIBBLADERF_LIBRARY_DIRS})
     list(APPEND LIBBLADERF_PC_PRIV_LIBS "-L${lib}")
 endforeach()
-
-if(ENABLE_BACKEND_LIBUSB)
-    # TODO set(LIBBLADERF_PC_REQUIRES ${LIBBLADERF_PC_REQUIRES} libusb-1.0 = ?)
-endif()
 
 set(LIBBLADERF_PC_PREFIX ${CMAKE_INSTALL_PREFIX})
 set(LIBBLADERF_PC_EXEC_PREFIX \${prefix})

host/libraries/libbladeRF/include/libbladeRF.h
@@ -71,6 +71,9 @@ typedef enum {
 
 /**
  * Information about a bladeRF attached to the system
+ *
+ * See the \ref FN_DEVINFO section for information on populating and comparing
+ * these structures.
  */
 struct bladerf_devinfo {
     bladerf_backend backend;    /**< Backend to use when connecting to device */
@@ -856,7 +859,8 @@ API_EXPORT int bladerf_is_fpga_configured(struct bladerf *dev);
  * @return 0 on success, value from \ref RETCODES list on failure
  */
 API_EXPORT int bladerf_get_fpga_version(struct bladerf *dev,
-                                        unsigned int *major, unsigned int *minor);
+                                        unsigned int *major,
+                                        unsigned int *minor);
 
 /**
  * Obtain device statistics
@@ -896,52 +900,31 @@ API_EXPORT int bladerf_flash_firmware(struct bladerf *dev,
  * This method recovers a bladeRF that is in the FX3 bootloader by loading the
  * specified firmware image.
  *
- * The general form of the device identifier string is;
- * @code
- *      <backend>:[device=<bus>:<addr>] [instance=<n>] [serial=<serial>]
- * @endcode
+ * @param[in]   device_identifier  Device identifier, formatted as described in
+ *                                  the bladerf_open() documentation
  *
- * An empty ("") or NULL device identifier will result in the first
- * encountered device being opened (using the first discovered backend)
- *
- * The 'backend' describes the mechanism used to communicate with the device,
- * and may be one of the following:
- *   - libusb:  libusb (See libusb changelog notes for required version, given
- *   your OS and controller)
- *   - linux:   Linux Kernel Driver
- *
- * If no arguments are provided after the backend, the first encountered
- * device on the specified backend will be opened. Note that a backend is
- * required, if any arguments are to be provided.
- *
- * Next, any provided arguments are provide as used to find the desired device.
- * Be sure not to over constrain the search. Generally, only one of the above
- * is required -- providing all of these may over constrain the search for the
- * desired device (e.g., if a serial number matches, but not on the specified
- * bus and address.)
- *
- *   - device=\<bus\>:\<addr\>
- *      - Specifies USB bus and address. Decimal or hex prefixed by '0x' is
- *        permitted.
- *   - instance=\<n\>
- *      - Nth instance encountered (libusb)
- *      - Device node N, such as /dev/bladerfN (linux)
- *   - serial=\<serial\>
- *      - Device's serial number.
- *
- * @param[in]   device_identifier  Device identifier, formatted as described above
- * @param[in]   fname              Filename of FX3 firmware load work
+ * @param[in]   fname              Filename of FX3 firmware to load
  *
  * @return 0 on success, or value from \ref RETCODES list on failure
  */
-API_EXPORT int bladerf_recover_with_devinfo(
-        struct bladerf_devinfo *devinfo,
-        const char *fname
-        );
-API_EXPORT int bladerf_recover(
-        const char *device_identifier,
-        const char *fname
-        );
+API_EXPORT int bladerf_recover(const char *device_identifier,
+                               const char *fname);
+
+/**
+ * Recover specified device using a device identifier information structure
+ *
+ * This method recovers a bladeRF that is in the FX3 bootloader by loading the
+ * specified firmware image.
+ *
+ * @param[in]   devinfo            Device identifier
+ *
+ * @param[in]   fname              Filename of FX3 firmware to load
+ *
+ * @return 0 on success, or value from \ref RETCODES list on failure
+ */
+API_EXPORT int bladerf_recover_with_devinfo(struct bladerf_devinfo *devinfo,
+                                            const char *fname);
+
 
 /**
  * Erase pages from FX3 flash device
@@ -1022,6 +1005,73 @@ API_EXPORT int bladerf_load_fpga(struct bladerf *dev, const char *fpga);
 
 
 /* @} (End of FN_PROG) */
+
+
+/**
+ * @defgroup FN_DEVINFO Device identifier information functions
+ * @{
+ */
+
+/**
+ * Initialize a device identifier information structure to a "wildcard" state.
+ * The values in each field will match any value for that field.
+ *
+ * Passing a bladerf_devinfo initialized with this function to
+ * bladerf_open_with_devinfo() will match the first device found.
+ */
+API_EXPORT void bladerf_init_devinfo(struct bladerf_devinfo *info);
+
+/**
+ * Fill out a provided bladerf_devinfo structure, given an open device handle.
+ *
+ * @pre dev must be a valid device handle.
+ *
+ * @param[in]    dev     Device handle previously obtained with bladerf_open()
+ * @param[out]   info    Device information populated by this function
+ *
+ * @return 0 on success, value from \ref RETCODES list on failure
+ */
+API_EXPORT int bladerf_get_devinfo(struct bladerf *dev,
+                                   struct bladerf_devinfo *info);
+
+/**
+ * Populate a device identifier information structure using the provided
+ * device identifier string.
+ *
+ * @param[in]   devstr  Device identifier string, formated as described
+ *                      in the bladerf_open() documentation
+ *
+ * @param[out]  info    Upon success, this will be filled out according to the
+ *                      provided device identifier string, with wildcards for
+ *                      any fields that were not provided.
+ *
+ * @return 0 on success, value from \ref RETCODES list on failure
+ */
+API_EXPORT int bladerf_get_devinfo_from_str(const char *devstr,
+                                            struct bladerf_devinfo *info);
+
+/**
+ * Test whether two device identifier information structures match, taking
+ * wildcard values into account.
+ */
+API_EXPORT bool bladerf_devinfo_matches(const struct bladerf_devinfo *a,
+                                        const struct bladerf_devinfo *b);
+
+/**
+ * Test whether a provided device string matches a device described by
+ * the provided bladerf_devinfo structure
+ *
+ * @param[in]   dev_str     Devices string, formated as described in the
+ *                          the documentation of bladerf_open
+ *
+ * @param[in]   info        Device info to compare with
+ *
+ * @return  true upon a match, false otherwise
+ */
+API_EXPORT bool bladerf_devstr_matches(const char *dev_str,
+                                       struct bladerf_devinfo *info);
+/** @} (End of FN_DEVINFO) */
+
 
 /**
  * @defgroup FN_MISC Miscellaneous

host/libraries/libbladeRF/src/backend/linux.c
@@ -493,6 +493,41 @@ int linux_close(struct bladerf *dev)
     }
 }
 
+static int linux_populate_devinfo(struct bladerf *dev,
+                                    struct bladerf_devinfo *devinfo,
+                                    unsigned int instance)
+{
+    int status, tmp;
+    struct bladerf_linux *backend = dev->backend;
+
+    devinfo->backend = BLADERF_BACKEND_LINUX;
+    devinfo->instance = instance;
+
+    /* Fetch USB bus and address */
+    status = ioctl(backend->fd, BLADE_GET_BUS, &tmp);
+    if (status < 0) {
+        status = BLADERF_ERR_IO;
+    } else {
+        assert(tmp >= 0 || tmp < (int)DEVINFO_INST_ANY);
+        devinfo->usb_bus = tmp;
+    }
+
+    status = ioctl(backend->fd, BLADE_GET_ADDR, &tmp);
+    if (status < 0) {
+        status = BLADERF_ERR_IO;
+    } else {
+        assert(tmp >= 0 && tmp < DEVINFO_ADDR_ANY);
+        devinfo->usb_addr = tmp;
+    }
+
+    /* Fetch device's serial # */
+    status = bladerf_read_serial(dev, (char *)&devinfo->serial);
+    if (status < 0) {
+        status = BLADERF_ERR_IO;
+    }
+
+    return status;
+}
 
 /* Forward declared so linux_open can use it for the wildcard case */
 static int linux_probe(struct bladerf_devinfo_list *info_list);
@@ -525,8 +560,13 @@ static int linux_open( struct bladerf **device, struct bladerf_devinfo *info)
                 ret->fn = &bladerf_linux_fn;
                 ret->backend = backend;
                 *device = ret;
-                status = 0;
-            } else {
+
+                /* Ensure all dev-info fields are written */
+                status =
+                    linux_populate_devinfo(ret, &ret->ident, info->instance);
+            }
+
+            if (!backend || !ret || status != 0) {
                 free(backend);
                 free(ret);
             }
@@ -637,59 +677,28 @@ static int linux_probe(struct bladerf_devinfo_list *info_list)
 {
     int status = 0;
     struct dirent **matches;
-    int num_matches, i, tmp;
+    int num_matches, i;
     struct bladerf *dev;
     struct bladerf_devinfo devinfo;
-    struct bladerf_linux *backend;
 
     num_matches = scandir(BLADERF_DEV_DIR, &matches, device_filter, alphasort);
     if (num_matches > 0) {
 
         for (i = 0; i < num_matches; i++) {
             status = 0;
+
+            /* Open this specific instance. */
             bladerf_init_devinfo(&devinfo);
-            devinfo.backend = BLADERF_BACKEND_LINUX;
             devinfo.instance = str2instance(matches[i]->d_name);
+
             status = linux_open(&dev, &devinfo);
-            backend = dev->backend;
 
-            if (!status) {
-
-                /* Fetch USB bus and address */
-                status = ioctl(backend->fd, BLADE_GET_BUS, &tmp);
-                if (status < 0) {
-                    log_warning("Failed to get bus. Skipping instance %d\n",
-                                devinfo.instance);
-                    status = BLADERF_ERR_IO;
-                } else {
-                    assert(tmp >= 0 || tmp < (int)DEVINFO_INST_ANY);
-                    devinfo.usb_bus = tmp;
-                }
-
-                status = ioctl(backend->fd, BLADE_GET_ADDR, &tmp);
-                if (status < 0) {
-                    log_warning("Failed to get addr. Skipping instance %d\n",
-                               devinfo.instance);
-                    status = BLADERF_ERR_IO;
-                } else {
-                    assert(tmp >= 0 && tmp < DEVINFO_ADDR_ANY);
-                    devinfo.usb_addr = tmp;
-                }
-
-                /* Fetch device's serial # */
-                status = bladerf_get_serial(dev, (char *)&devinfo.serial);
-                if (status < 0) {
-                    log_warning("Failed to get serial. Skipping instance %d\n",
-                               devinfo.instance);
-                    status = BLADERF_ERR_IO;
-                }
-
-            } else {
+            if (status < 0) {
                 log_error("Failed to open instance=%d\n", devinfo.instance);
-            }
-
-            if (status == 0) {
-                bladerf_devinfo_list_add(info_list, &devinfo);
+            } else {
+                /* Since this device was opened by instance, it will have
+                 * had it's device info (dev->ident) filled out already */
+                bladerf_devinfo_list_add(info_list, &dev->ident);
             }
             linux_close(dev);
         }

host/libraries/libbladeRF/src/bladerf.c
@@ -106,11 +106,6 @@ int bladerf_open(struct bladerf **device, const char *dev_id)
     if (!status) {
         if (!dev->legacy) {
 
-            status = bladerf_get_and_cache_serial(dev);
-            if (status < 0) {
-                log_warning( "Could not extract serial number\n" ) ;
-            }
-
             status = bladerf_get_and_cache_vctcxo_trim(dev);
             if (status < 0) {
                 log_warning( "Could not extract VCTCXO trim value\n" ) ;
@@ -126,7 +121,7 @@ int bladerf_open(struct bladerf **device, const char *dev_id)
              * print here (i.e., not uninitialized) */
             log_debug("%s: fw=v%d.%d serial=%s trim=0x%.4x fpga_size=%d\n",
                     __FUNCTION__, dev->fw_major, dev->fw_minor,
-                    dev->serial, dev->dac_trim, dev->fpga_size);
+                    dev->ident.serial, dev->dac_trim, dev->fpga_size);
         } else {
             printf("********************************************************************************\n");
             printf("* ENTERING LEGACY MODE, PLEASE UPGRADE TO THE LATEST FIRMWARE BY RUNNING:\n");
@@ -619,7 +614,7 @@ int bladerf_stream(struct bladerf_stream *stream, bladerf_module module)
 
 int bladerf_get_serial(struct bladerf *dev, char *serial)
 {
-    strcpy(serial, dev->serial);
+    strcpy(serial, dev->ident.serial);
     return 0;
 }
 
@@ -861,6 +856,66 @@ const char * bladerf_version(unsigned int *major,
 bladerf_log_level bladerf_log_set_verbosity(bladerf_log_level level)
 {
     return log_set_verbosity(level);
+}
+
+/*------------------------------------------------------------------------------
+ * Device identifier information
+ *----------------------------------------------------------------------------*/
+
+void bladerf_init_devinfo(struct bladerf_devinfo *info)
+{
+    info->backend  = BLADERF_BACKEND_ANY;
+
+    memset(info->serial, 0, BLADERF_SERIAL_LENGTH);
+    strncpy(info->serial, DEVINFO_SERIAL_ANY, BLADERF_SERIAL_LENGTH - 1);
+
+    info->usb_bus  = DEVINFO_BUS_ANY;
+    info->usb_addr = DEVINFO_ADDR_ANY;
+    info->instance = DEVINFO_INST_ANY;
+}
+
+int bladerf_get_devinfo(struct bladerf *dev, struct bladerf_devinfo *info)
+{
+    if (dev) {
+        memcpy(info, &dev->ident, sizeof(struct bladerf_devinfo));
+        return 0;
+    } else {
+        return BLADERF_ERR_INVAL;
+    }
+}
+
+int bladerf_get_devinfo_from_str(const char *devstr,
+                                 struct bladerf_devinfo *info)
+{
+    return str2devinfo(devstr, info);
+}
+
+bool bladerf_devinfo_matches(const struct bladerf_devinfo *a,
+                             const struct bladerf_devinfo *b)
+{
+    return bladerf_instance_matches(a, b) &&
+           bladerf_serial_matches(a, b)   &&
+           bladerf_bus_addr_matches(a ,b);
+}
+
+
+bool bladerf_devstr_matches(const char *dev_str,
+                            struct bladerf_devinfo *info)
+{
+    int status;
+    bool ret;
+    struct bladerf_devinfo from_str;
+
+    status = str2devinfo(dev_str, &from_str);
+    if (status < 0) {
+        ret = false;
+        log_error("Failed to parse device string: %s\n",
+                  bladerf_strerror(status));
+    } else {
+        ret = bladerf_devinfo_matches(&from_str, info);
+    }
+
+    return ret;
 }
 
 /*------------------------------------------------------------------------------

host/libraries/libbladeRF/src/bladerf_priv.c
@@ -150,6 +150,8 @@ int bladerf_get_otp_field(struct bladerf *dev, char *field,
     int status;
     char otp[OTP_BUFFER_SIZE];
 
+    memset(otp, 0xff, OTP_BUFFER_SIZE);
+
     status = dev->fn->get_otp(dev, otp);
     if (status < 0)
         return status;
@@ -170,16 +172,22 @@ int bladerf_get_cal_field(struct bladerf *dev, char *field,
         return extract_field(cal, CAL_BUFFER_SIZE, field, data, data_size);
 }
 
-int bladerf_get_and_cache_serial(struct bladerf *dev)
+int bladerf_read_serial(struct bladerf *dev, char *serial_buf)
 {
     int status;
-    status = bladerf_get_otp_field(dev, "S", dev->serial,
+
+    status = bladerf_get_otp_field(dev, "S", serial_buf,
                                     BLADERF_SERIAL_LENGTH - 1);
 
     if (status < 0) {
-        log_debug("Unable to fetch serial number. Defaulting to 0's\n");
-        memset(dev->serial, 0, BLADERF_SERIAL_LENGTH);
+        log_error("Unable to fetch serial number. Defaulting to 0's.\n");
+        memset(dev->ident.serial, '0', BLADERF_SERIAL_LENGTH - 1);
+
+        /* Treat this as non-fatal */
+        status = 0;
     }
+
+    serial_buf[BLADERF_SERIAL_LENGTH - 1] = '\0';
 
     return status;
 }

host/libraries/libbladeRF/src/bladerf_priv.h
@@ -9,7 +9,7 @@
 #include "host_config.h"
 #include "minmax.h"
 #include "conversions.h"
-#include "bladerf_devinfo.h"
+#include "devinfo.h"
 
 /* MX25U3235 - 32Mbit flash w/ 4KiB sectors */
 #define FLASH_SECTOR_SIZE   0x10000
@@ -75,7 +75,9 @@ struct bladerf_fn {
      * bladerf_devinfo_list_append() */
     int (*probe)(struct bladerf_devinfo_list *info_list);
 
-    /* Opening device based upon specified device info*/
+    /* Opening device based upon specified device info
+     * devinfo structure. The implementation of this function is responsible
+     * for ensuring (*device)->ident is populated */
     int (*open)(struct bladerf **device,  struct bladerf_devinfo *info);
 
     /* Closing of the device and freeing of the data */
@@ -135,8 +137,11 @@ struct bladerf_fn {
 #define FW_LEGACY_ALT_SETTING_MAJOR 1
 #define FW_LEGACY_ALT_SETTING_MINOR 1
 #define LEGACY_ALT_SETTING  1
+
 struct bladerf {
-    char serial[BLADERF_SERIAL_LENGTH]; /* The device's serial number */
+
+    struct bladerf_devinfo ident;  /* Identifying information */
+
     uint16_t dac_trim;
     bladerf_fpga_size fpga_size;
 
@@ -146,15 +151,10 @@ struct bladerf {
     int speed;      /* The device's USB speed, 0 is HS, 1 is SS */
     struct bladerf_stats stats;
 
-    /* FIXME temporary workaround for not being able to read back sample rate */
-    unsigned int last_tx_sample_rate;
-    unsigned int last_rx_sample_rate;
-
     /* Last error encountered */
     struct bladerf_error error;
 
-    /* Type of the underlying driver and its private data  */
-    bladerf_backend backend_type;
+    /* Backend's private data  */
     void *backend;
 
     /* Driver-sppecific implementations */
@@ -213,14 +213,15 @@ int bladerf_get_otp_field(struct bladerf *device, char *field,
                             char *data, size_t data_size);
 
 /**
- * Retrieve the device serial from flash and cache it in the provided
- * device structure
+ * Retrieve the device serial from flash and store it in the provided buffer.
+ *
+ * @pre The provided buffer is BLADERF_SERIAL_LENGTH in size
  *
  * @param[inout]   dev      Device handle. On success, serial field is updated
  *
  * 0 on success, BLADERF_ERR_* on failure
  */
-int bladerf_get_and_cache_serial(struct bladerf *device);
+int bladerf_read_serial(struct bladerf *device, char *serial_buf);
 
 /**
  * Retrieve VCTCXO calibration value from flash and cache it in the

host/libraries/libbladeRF/src/device_identifier.c
@@ -10,7 +10,7 @@
 #include <stdint.h>
 
 #include "device_identifier.h"
-#include "bladerf_devinfo.h"
+#include "devinfo.h"
 #include "conversions.h"
 #include "log.h"
 

commit 7a5961688e2eec5053f6421ad9e172d5d7b6827a

libbladeRF: Fixed use of uninitialized data (libusb backend)
Fixed defect introduced in 9015bdbb5ca2336a94f41a8575c67e7862fa8073, where a device handle's device identifier information was being populated from a (most likely) uninitialized bladerf_devinfo, rather than the one fully populated by a call to lusb_get_devinfo().

host/libraries/libbladeRF/src/backend/libusb.c
@@ -343,7 +343,7 @@ static int lusb_open(struct bladerf **device, struct bladerf_devinfo *info)
                 dev->backend = (void *)lusb;
                 dev->legacy = 0;
 
-                memcpy(&dev->ident, &info, sizeof(struct bladerf_devinfo));
+                memcpy(&dev->ident, &thisinfo, sizeof(struct bladerf_devinfo));
 
                 /* Populate the backend information */
                 lusb->context = context;