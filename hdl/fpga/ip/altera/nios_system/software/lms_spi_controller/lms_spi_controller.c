d90dad8f748eb067959cd90ed3b70f2d6f8d998f
Ryan Tucker <rtucker@gmail.com>
2013-09-22 11:17:08 -0400

    lms_spi_controller: Remove inaccurate header comment

--- hdl/fpga/ip/altera/nios_system/software/lms_spi_controller/lms_spi_controller.c
+++ hdl/fpga/ip/altera/nios_system/software/lms_spi_controller/lms_spi_controller.c
@@ -1,83 +1,3 @@
-/*
- * "Small Hello World" example.
- *
- * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
- * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
- * designs. It requires a STDOUT  device in your system's hardware.
- *
- * The purpose of this example is to demonstrate the smallest possible Hello
- * World application, using the Nios II HAL library.  The memory footprint
- * of this hosted application is ~332 bytes by default using the standard
- * reference design.  For a more fully featured Hello World application
- * example, see the example titled "Hello World".
- *
- * The memory footprint of this example has been reduced by making the
- * following changes to the normal "Hello World" example.
- * Check in the Nios II Software Developers Manual for a more complete
- * description.
- *
- * In the SW Application project (small_hello_world):
- *
- *  - In the C/C++ Build page
- *
- *    - Set the Optimization Level to -Os
- *
- * In System Library project (small_hello_world_syslib):
- *  - In the C/C++ Build page
- *
- *    - Set the Optimization Level to -Os
- *
- *    - Define the preprocessor option ALT_NO_INSTRUCTION_EMULATION
- *      This removes software exception handling, which means that you cannot
- *      run code compiled for Nios II cpu with a hardware multiplier on a core
- *      without a the multiply unit. Check the Nios II Software Developers
- *      Manual for more details.
- *
- *  - In the System Library page:
- *    - Set Periodic system timer and Timestamp timer to none
- *      This prevents the automatic inclusion of the timer driver.
- *
- *    - Set Max file descriptors to 4
- *      This reduces the size of the file handle pool.
- *
- *    - Check Main function does not exit
- *    - Uncheck Clean exit (flush buffers)
- *      This removes the unneeded call to exit when main returns, since it
- *      won't.
- *
- *    - Check Don't use C++
- *      This builds without the C++ support code.
- *
- *    - Check Small C library
- *      This uses a reduced functionality C library, which lacks
- *      support for buffering, file IO, floating point and getch(), etc.
- *      Check the Nios II Software Developers Manual for a complete list.
- *
- *    - Check Reduced device drivers
- *      This uses reduced functionality drivers if they're available. For the
- *      standard design this means you get polled UART and JTAG UART drivers,
- *      no support for the LCD driver and you lose the ability to program
- *      CFI compliant flash devices.
- *
- *    - Check Access device drivers directly
- *      This bypasses the device file system to access device drivers directly.
- *      This eliminates the space required for the device file system services.
- *      It also provides a HAL version of libc services that access the drivers
- *      directly, further reducing space. Only a limited number of libc
- *      functions are available in this configuration.
- *
- *    - Use ALT versions of stdio routines:
- *
- *           Function                  Description
- *        ===============  =====================================
- *        alt_printf       Only supports %s, %x, and %c ( < 1 Kbyte)
- *        alt_putstr       Smaller overhead than puts with direct drivers
- *                         Note this function doesn't add a newline.
- *        alt_putchar      Smaller overhead than putchar with direct drivers
- *        alt_getchar      Smaller overhead than getchar with direct drivers
- *
- */
-
 #include "sys/alt_stdio.h"
 #include "system.h"
 #include "altera_avalon_spi.h"
@@ -100,7 +20,6 @@
 #define kHz(x) (x*1000)
 #define MHz(x) (x*1000000)
 #define GHz(x) (x*1000000000)
-
 
 // Register offsets from the base
 #define I2C 				OC_I2C_MASTER_0_BASE
@@ -168,8 +87,6 @@ void si5338_write( uint8_t addr, uint8_t data ) {
 	return ;
 }
 
-
-
 // Trim DAC write
 void dac_write( uint16_t val ) {
 //	alt_printf( "DAC Writing: %x\n", val ) ;
@@ -219,7 +136,6 @@ void lms_spi_write( uint8_t address, uint8_t val )
 	}
 	return ;
 }
-
 
 const char msg[] = "bladeRF FSK example!\n" ;
 
