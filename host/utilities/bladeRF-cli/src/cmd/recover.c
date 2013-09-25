46e261796d2e4208f428355e39a18bb90411c11e
litghost <litghost@softhome.net>
2013-09-24 21:10:38 -0700

    Add variable expansion support to recover function

--- host/utilities/bladeRF-cli/src/cmd/recover.c
+++ host/utilities/bladeRF-cli/src/cmd/recover.c
@@ -3,16 +3,26 @@
 #include <stdbool.h>
 #include <conversions.h>
 #include "cmd.h"
+#include "interactive.h"
 
 int cmd_recover(struct cli_state *state, int argc, char **argv)
 {
     int status;
+    char *dev_id, *path, *expanded_path;
 
     if (argc != 3) {
         return CMD_RET_NARGS;
     }
 
-    status = bladerf_recover(argv[1], argv[2]);
+    dev_id = argv[1];
+    path = argv[2];
+
+    if ((expanded_path = interactive_expand_path(path)) == NULL) {
+        cli_err(state, "Unable to expand FPGA file path: \"%s\"", path);
+        return CMD_RET_INVPARAM;
+    }
+
+    status = bladerf_recover(dev_id, expanded_path);
 
     if (status == 0) {
         printf("Loaded! An open is now required\n");
