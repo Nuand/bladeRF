26e8f3c0c0bf176144c2cd7437751e527915b768
Brian Padalino <bpadalino@gmail.com>
2013-09-24 23:31:03 -0400

    libbladeRF: Moved where legacy warning is printed.

--- host/libraries/libbladeRF/src/bladerf.c
+++ host/libraries/libbladeRF/src/bladerf.c
@@ -103,6 +103,19 @@ int bladerf_open(struct bladerf **device, const char *dev_id)
         dev = *device;
     }
 
+    if (dev->legacy) {
+        /* Currently two modes of legacy:
+         *  - ALT_SETTING
+         *  - CONFIG_IF
+         *
+         * If either of these are set, we should tell the user to update
+         */
+        printf("********************************************************************************\n");
+        printf("* ENTERING LEGACY MODE, PLEASE UPGRADE TO THE LATEST FIRMWARE BY RUNNING:\n");
+        printf("* wget http://nuand.com/fx3/latest.img ; bladeRF-cli -f latest.img\n");
+        printf("********************************************************************************\n");
+    }
+
     if (!status) {
         if (!(dev->legacy & LEGACY_ALT_SETTING)) {
 
@@ -122,16 +135,11 @@ int bladerf_open(struct bladerf **device, const char *dev_id)
             log_debug("%s: fw=v%d.%d serial=%s trim=0x%.4x fpga_size=%d\n",
                     __FUNCTION__, dev->fw_major, dev->fw_minor,
                     dev->ident.serial, dev->dac_trim, dev->fpga_size);
-        } else {
-            printf("********************************************************************************\n");
-            printf("* ENTERING LEGACY MODE, PLEASE UPGRADE TO THE LATEST FIRMWARE BY RUNNING:\n");
-            printf("* wget http://nuand.com/fx3/latest.img ; bladeRF-cli -f latest.img\n");
-            printf("********************************************************************************\n");
         }
-
-        /* All status in here is not fatal, so whatever */
-        status = 0 ;
     }
+
+    /* All status in here is not fatal, so whatever */
+    status = 0 ;
 
     return status;
 }
