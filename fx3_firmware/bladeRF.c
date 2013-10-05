Nuand/bladeRF

commit 58c13c65a88e67b75b640793f948a1d73e9ff21d

fx3_firmware, libbladeRF: Packed FW version structure, moved serial # init
The bladerf_fx3_version structure is transferred as-is. Currently this
likely doesn't pose any isseus for most machines, since it contains only
two uint32_t's. It's now marked to be packed, just to be safe.

The serial number initialization (ExtracSerial()) has been moved before
the CyU3PConnectState() call to remove a potential race condition.

Macro definitions are now provided for string descriptors.

fx3_firmware/CMakeLists.txt
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 2.8.3)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../host/cmake/modules)
 set(VERSION_INFO_MAJOR 1)
 set(VERSION_INFO_MINOR 5)
-set(VERSION_INFO_PATCH 0)
+set(VERSION_INFO_PATCH 1)
 set(VERSION_INFO_EXTRA "git")
 include(Version)
 

fx3_firmware/bladeRF.c
@@ -809,28 +809,43 @@ void bladeRFInit(void)
         CyFxAppErrorHandler(apiRetStatus);
     }
 
-    /* String descriptor 1 */
-    apiRetStatus = CyU3PUsbSetDesc(CY_U3P_USB_SET_STRING_DESCR, 1, (uint8_t *)CyFxUSBManufactureDscr);
+    /* Manufacturer string descriptor */
+    apiRetStatus = CyU3PUsbSetDesc(CY_U3P_USB_SET_STRING_DESCR,
+                                   BLADE_USB_STR_INDEX_MFR,
+                                   (uint8_t *)CyFxUSBManufactureDscr);
+
     if (apiRetStatus != CY_U3P_SUCCESS) {
         CyU3PDebugPrint (4, "USB set string descriptor failed, Error code = %d\n", apiRetStatus);
         CyFxAppErrorHandler(apiRetStatus);
     }
 
-    /* String descriptor 2 */
-    apiRetStatus = CyU3PUsbSetDesc(CY_U3P_USB_SET_STRING_DESCR, 2, (uint8_t *)CyFxUSBProductDscr);
+    /* Product string descriptor */
+    apiRetStatus = CyU3PUsbSetDesc(CY_U3P_USB_SET_STRING_DESCR,
+                                   BLADE_USB_STR_INDEX_PRODUCT,
+                                   (uint8_t *)CyFxUSBProductDscr);
+
     if (apiRetStatus != CY_U3P_SUCCESS) {
         CyU3PDebugPrint (4, "USB set string descriptor failed, Error code = %d\n", apiRetStatus);
         CyFxAppErrorHandler(apiRetStatus);
     }
 
-    /* String descriptor 3 */
-    apiRetStatus = CyU3PUsbSetDesc(CY_U3P_USB_SET_STRING_DESCR, 3, (uint8_t *)CyFxUSBSerial);
+    /* Fetch serial number from flash and configure the serial number
+     * string descriptor */
+    ExtractSerial();
+    apiRetStatus = CyU3PUsbSetDesc(CY_U3P_USB_SET_STRING_DESCR,
+                                   BLADE_USB_STR_INDEX_PRODUCT,
+                                   (uint8_t *)CyFxUSBSerial);
+
     if (apiRetStatus != CY_U3P_SUCCESS) {
         CyU3PDebugPrint (4, "USB set string descriptor failed, Error code = %d\n", apiRetStatus);
         CyFxAppErrorHandler(apiRetStatus);
     }
 
-    apiRetStatus = CyU3PUsbSetDesc(CY_U3P_USB_SET_STRING_DESCR, 4, (uint8_t *)CyFxUSBVersion);
+    /* Firmware version string descriptor */
+    apiRetStatus = CyU3PUsbSetDesc(CY_U3P_USB_SET_STRING_DESCR,
+                                  BLADE_USB_STR_INDEX_FW_VER,
+                                  (uint8_t *)CyFxUSBVersion);
+
     if (apiRetStatus != CY_U3P_SUCCESS) {
         CyU3PDebugPrint (4, "USB set descriptor failed for version string, Error code = %d\n", apiRetStatus);
         CyFxAppErrorHandler(apiRetStatus);
@@ -882,8 +897,6 @@ void bladeRFAppThread_Entry( uint32_t input)
     populateVersionString();
 
     bladeRFInit();
-
-    ExtractSerial();
 
     FpgaBeginProgram();
     for (;;) {

host/libraries/libbladeRF/CMakeLists.txt
@@ -15,7 +15,7 @@ project(libbladeRF C)
 
 set(VERSION_INFO_MAJOR  0)
 set(VERSION_INFO_MINOR  6)
-set(VERSION_INFO_PATCH  0)
+set(VERSION_INFO_PATCH  1)
 set(VERSION_INFO_EXTRA "git")
 include(Version)
 

firmware_common/bladeRF.h
@@ -50,15 +50,32 @@
 #define BLADE_USB_CMD_INVALIDATE_CAL_CACHE    111
 #define BLADE_USB_CMD_REFRESH_CAL_CACHE       112
 
-#define BLADE_USB_STR_INDEX_DEV_VER 4
+/* String descriptor indices */
+#define BLADE_USB_STR_INDEX_MFR     1   /* Manufacturer */
+#define BLADE_USB_STR_INDEX_PRODUCT 2   /* Product */
+#define BLADE_USB_STR_INDEX_SERIAL  3   /* Serial number */
+#define BLADE_USB_STR_INDEX_FW_VER  4   /* Firmware version */
 
 #define CAL_BUFFER_SIZE 256
 #define CAL_PAGE 768
 
+#ifdef _MSC_VER
+#   define PACK(decl_to_pack_) \
+            __pragma(pack(push,1)) \
+            decl_to_pack_ \
+            __pragma(pack(pop))
+#elif defined(__GNUC__)
+#   define PACK(decl_to_pack_) \
+            decl_to_pack_ __attribute__((__packed__))
+#else
+#error "Unexpected compiler/environment"
+#endif
+
+PACK(
 struct bladerf_fx3_version {
     unsigned short major;
     unsigned short minor;
-};
+});
 
 struct bladeRF_firmware {
     unsigned int len;

host/libraries/libbladeRF/src/backend/libusb.c
@@ -1362,7 +1362,7 @@ static int lusb_fw_version(struct bladerf *dev,
     struct bladerf_lusb *lusb = dev->backend;
 
     status = libusb_get_string_descriptor_ascii(lusb->handle,
-                                                BLADE_USB_STR_INDEX_DEV_VER,
+                                                BLADE_USB_STR_INDEX_FW_VER,
                                                 (unsigned char *)dev->fw_version_str,
                                                 BLADERF_VERSION_STR_MAX);
 

commit cf552e30e53e51d85bd45c7159011267a58d3c07

fx3_firmware: Fixed string index  error introduced in 58c13c65a88e

fx3_firmware/CMakeLists.txt
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 2.8.3)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../host/cmake/modules)
 set(VERSION_INFO_MAJOR 1)
 set(VERSION_INFO_MINOR 5)
-set(VERSION_INFO_PATCH 2)
+set(VERSION_INFO_PATCH 3)
 set(VERSION_INFO_EXTRA "git")
 include(Version)
 

fx3_firmware/bladeRF.c
@@ -857,7 +857,7 @@ void bladeRFInit(void)
 
     /* Configure the serial number string descriptor */
     apiRetStatus = CyU3PUsbSetDesc(CY_U3P_USB_SET_STRING_DESCR,
-                                   BLADE_USB_STR_INDEX_PRODUCT,
+                                   BLADE_USB_STR_INDEX_SERIAL,
                                    (uint8_t *)CyFxUSBSerial);
 
     if (apiRetStatus != CY_U3P_SUCCESS) {

host/utilities/bladeRF-cli/CMakeLists.txt
@@ -11,8 +11,8 @@ project(bladeRF-cli C)
 ################################################################################
 
 set(VERSION_INFO_MAJOR  0)
-set(VERSION_INFO_MINOR  4)
-set(VERSION_INFO_PATCH  2)
+set(VERSION_INFO_MINOR  5)
+set(VERSION_INFO_PATCH  0)
 set(VERSION_INFO_EXTRA "git")
 include(Version)
 
@@ -85,6 +85,7 @@ set(BLADERF_CLI_SOURCE
         src/cmd/calibrate.c
         src/cmd/cmd.c
         src/cmd/erase.c
+        src/cmd/info.c
         src/cmd/load.c
         src/cmd/open.c
         src/cmd/peek.c

host/utilities/bladeRF-cli/src/cmd/cmd.c
@@ -8,6 +8,7 @@
 DECLARE_CMD(calibrate);
 DECLARE_CMD(clear);
 DECLARE_CMD(help);
+DECLARE_CMD(info);
 DECLARE_CMD(load);
 DECLARE_CMD(open);
 DECLARE_CMD(peek);
@@ -33,6 +34,7 @@ struct cmd {
 static const char *cmd_names_calibrate[] = { "calibrate", "cal", NULL };
 static const char *cmd_names_clear[] = { "clear", "cls", NULL };
 static const char *cmd_names_help[] = { "help", "h", "?", NULL };
+static const char *cmd_names_info[] = { "info", "i", NULL };
 static const char *cmd_names_load[] = { "load", "ld", NULL };
 static const char *cmd_names_open[] = { "open", "op", "o", NULL };
 static const char *cmd_names_peek[] = { "peek", "pe", NULL };
@@ -218,6 +220,21 @@ static const struct cmd cmd_table[] = {
             "help <command>\n"
             "\n"
             "Provides extended help, like this, on any command.\n"
+        )
+    },
+    {
+        FIELD_INIT(.names, cmd_names_info),
+        FIELD_INIT(.exec, cmd_info),
+        FIELD_INIT(.desc, "Print information about the currently opened device"),
+        FIELD_INIT(.help,
+                "info\n"
+                "\n"
+                "Prints the following information about an opened device:\n"
+                "  Serial number\n"
+                "  VCTCXO DAC calibration value\n"
+                "  FPGA size\n"
+                "  Whether or not the FPGA is loaded\n"
+                "  USB bus and address\n"
         )
     },
     {

host/utilities/bladeRF-cli/src/cmd/version.c
@@ -5,9 +5,6 @@
 int cmd_version(struct cli_state *state, int argc, char **argv)
 {
     int status;
-    char serial[BLADERF_SERIAL_LENGTH] = { 0 };
-    bladerf_fpga_size fpga_size;
-    uint16_t dac_trim;
 
     struct bladerf_version fw_version, fpga_version, lib_version;
     bool fpga_loaded = false;
@@ -19,41 +16,31 @@ int cmd_version(struct cli_state *state, int argc, char **argv)
     printf("  libbladeRF version:         %s\n", lib_version.describe);
     printf("\n");
 
+    /* Exit cleanly if no device is attached */
     if (state->dev == NULL) {
-        printf("  No device attached to retrive version information from.\n\n");
+        printf("  Device version information unavailable: No device attached.\n");
         return 0;
     }
 
     status = bladerf_is_fpga_configured(state->dev);
     if (status < 0) {
-        return status;
-    } else if (status) {
+        state->last_lib_error = status;
+        return CMD_RET_LIBBLADERF;
+    } else if (status != 0) {
         fpga_loaded = true;
         status = bladerf_fpga_version(state->dev, &fpga_version);
         if (status < 0) {
-            return status;
+            state->last_lib_error = status;
+            return CMD_RET_LIBBLADERF;
         }
     }
 
     status = bladerf_fw_version(state->dev, &fw_version);
     if (status < 0) {
-        return status;
+        state->last_lib_error = status;
+        return CMD_RET_LIBBLADERF;
     }
 
-    status = bladerf_get_serial(state->dev, serial);
-    if (status < 0) {
-        return status;
-    }
-
-    status = bladerf_get_fpga_size(state->dev, &fpga_size);
-    if (status < 0) {
-        return status;
-    }
-
-    status = bladerf_get_vctcxo_trim(state->dev, &dac_trim);
-    if (status < 0) {
-        return status;
-    }
 
     printf("  Firmware version:           %s\n", fw_version.describe);
 
@@ -63,17 +50,7 @@ int cmd_version(struct cli_state *state, int argc, char **argv)
         printf("  FPGA version:               Unknown (FPGA not loaded)\n");
     }
 
-    /* TODO: Move these into an info command */
-    printf("  Serial #:                   %s\n", serial);
-    printf("  VCTCXO DAC calibration:     0x%.4x\n", dac_trim);
-    if (fpga_size != 0) {
-        printf("  FPGA size:                  %d KLE\n", fpga_size);
-    } else {
-        printf("  FPGA size:                  Unknown\n");
-    }
     printf("\n");
-
-
     return CMD_RET_OK;
 }
 