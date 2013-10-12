a4e4c4b0247bf1ae8eb5705eedba9f140dd9b531
Jon Szymaniak <jon.szymaniak@gmail.com>
2013-10-11 23:13:26 -0400

    license: License changes on various portions of the codebase
    Where possible (and with contributor permission), items have been
    changed to LGPLv2.1 or MIT licenses to avoid issues with any proprietary
    libraries from vendors (HDL and FW), allow static linking (with headless
    mode operation in mind), and to generally present users with more
    permissive licenses.
    
    File headers and the COPYING have been updated accordingly. Third party
    sources have been either removed, replaced, or relocated in the source
    tree, in accordance with the licenses decided upon for each part of the tree.
    
    Contributor Assignment Agreements have been added, and it is required
    that these be submitted before significant patches and pull requests
    are accepted.

--- host/libraries/libbladeRF/src/device_identifier.h
+++ host/libraries/libbladeRF/src/device_identifier.h
@@ -1,5 +1,26 @@
 /**
- * Routines for parsing and handling device identifier strings
+ * @file device_identifier.h
+ *
+ * @brief Routines for parsing and handling device identifier strings
+ *
+ * This file is part of the bladeRF project:
+ *   http://www.github.com/nuand/bladeRF
+ *
+ * Copyright (C) 2013 Nuand LLC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 #ifndef DEVICE_IDENTIFIER_H__
 #define DEVICE_IDENTIFIER_H__
--- host/libraries/libbladeRF/src/file_ops.h
+++ host/libraries/libbladeRF/src/file_ops.h
@@ -1,6 +1,29 @@
 /**
- * Wrappers around misc. file operations. These are collected here so that
- * they may easily be dummied out for NIOS headless builds in the future.
+ * @file file_ops.h
+ *
+ * @brief Wrappers around misc. file operations.
+ *
+ * These are collected here so that they may easily be dummied out for NIOS
+ * headless builds in the future.
+ *
+ * This file is part of the bladeRF project:
+ *   http://www.github.com/nuand/bladeRF
+ *
+ * Copyright (C) 2013 Nuand LLC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 #ifndef FILE_OPS_H__
 #define FILE_OPS_H__
