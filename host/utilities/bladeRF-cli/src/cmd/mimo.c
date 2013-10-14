b87d123218158702696c01e0a59fd63259e3dbf2
Brian Padalino <bpadalino@gmail.com>
2013-10-14 18:25:45 -0400

    cli: Add string.h and remove some tabs.

--- host/utilities/bladeRF-cli/src/cmd/mimo.c
+++ host/utilities/bladeRF-cli/src/cmd/mimo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "common.h"
+#include <string.h>
 
 int cmd_mimo(struct cli_state *state, int argc, char **argv)
 {
@@ -10,26 +11,26 @@ int cmd_mimo(struct cli_state *state, int argc, char **argv)
         return 0;
     }
 
-	if (argc >= 2) {
-		if (argc >= 3 && !strcmp(argv[1], "clk")) {
-			if (!strcmp(argv[2], "slave")) {
-				status |= bladerf_si5338_write(state->dev, 6, 4);
-				status |= bladerf_si5338_write(state->dev, 28, 0x2b);
-				status |= bladerf_si5338_write(state->dev, 29, 0x28);
-				status |= bladerf_si5338_write(state->dev, 30, 0xa8);
-				if (status)
-					printf("Could not set device to slave MIMO mode\n");
-				else
-					printf("Successfully set device to slave MIMO mode\n");
-			} else if (!strcmp(argv[2], "master")) {
-				status |= bladerf_si5338_write(state->dev, 39, 1);
-				status |= bladerf_si5338_write(state->dev, 34, 0x22);
-				if (status)
-					printf("Could not set device to master MIMO mode\n");
-				else
-					printf("Successfully set device to master MIMO mode\n");
-			}
-		}
-	}
+    if (argc >= 2) {
+        if (argc >= 3 && !strcmp(argv[1], "clk")) {
+            if (!strcmp(argv[2], "slave")) {
+                status |= bladerf_si5338_write(state->dev, 6, 4);
+                status |= bladerf_si5338_write(state->dev, 28, 0x2b);
+                status |= bladerf_si5338_write(state->dev, 29, 0x28);
+                status |= bladerf_si5338_write(state->dev, 30, 0xa8);
+                if (status)
+                    printf("Could not set device to slave MIMO mode\n");
+                else
+                    printf("Successfully set device to slave MIMO mode\n");
+            } else if (!strcmp(argv[2], "master")) {
+                status |= bladerf_si5338_write(state->dev, 39, 1);
+                status |= bladerf_si5338_write(state->dev, 34, 0x22);
+                if (status)
+                    printf("Could not set device to master MIMO mode\n");
+                else
+                    printf("Successfully set device to master MIMO mode\n");
+            }
+        }
+    }
     return status;
 }
