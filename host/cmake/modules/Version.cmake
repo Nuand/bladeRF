98eca856f9f78a218f51b7f7be63ce493d8d8a40
Ryan Tucker <rtucker@gmail.com>
2013-10-27 15:51:32 -0400

    Inject the current Debian snapshot version
    This figures out the version number of the source package we're
    building, then injects it into the VERSION_INFO within CMake.

--- host/cmake/modules/Version.cmake
+++ host/cmake/modules/Version.cmake
@@ -3,17 +3,22 @@
 #   http://sdr.osmocom.org/trac/wiki/rtl-sdr
 #
 # Provides:
-#   ${VERSION_INFO_BASE}  -  Major.Minor.Patch
-#   ${VERSION_INFO}       -  Major.minor.Patch[-git_info] 
+#   ${VERSION_INFO_BASE}     -  Major.Minor.Patch
+#   ${VERSION_INFO}          -  Major.minor.Patch[-git_info]
 #
 # Requires values for:
-#   ${VERSION_INFO_MAJOR} - Increment on API compatibility changes.
-#   ${VERSION_INFO_MINOR} - Increment when adding features.
-#   ${VERSION_INFO_PATCH} - Increment for bug and documenation changes.
+#   ${VERSION_INFO_MAJOR}    - Increment on API compatibility changes.
+#   ${VERSION_INFO_MINOR}    - Increment when adding features.
+#   ${VERSION_INFO_PATCH}    - Increment for bug and documentation changes.
 #
 # Optional:
-#   ${VERSION_INFO_EXTRA} - Set to "git" to append git info. This is intended
-#                           only non-versioned development builds
+#   ${VERSION_INFO_EXTRA}    - Set to "git" to append git info. This is
+#                              intended only for non-versioned development
+#                              builds
+#   ${VERSION_INFO_OVERRIDE} - Set to a non-null value to override the
+#                              VERSION_INFO_EXTRA logic. This is intended
+#                              for automated snapshot builds from exported
+#                              trees, to pass in the git revision info.
 #
 if(DEFINED __INCLUDED_BLADERF_VERSION_CMAKE)
     return()
@@ -50,25 +55,37 @@ find_package(Git QUIET)
 
 if(GIT_FOUND)
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD -- 
-        OUTPUT_VARIABLE GIT_REV OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND ${GIT_EXECUTABLE} rev-parse --
+        ERROR_QUIET
+        RESULT_VARIABLE NOT_GIT_REPOSITORY
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     )
 
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} diff-index --quiet HEAD -- 
-        RESULT_VARIABLE GIT_DIRTY
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    )
-
-    if(GIT_DIRTY)
-        set(GIT_INFO "-${GIT_REV}-dirty")
+    if(NOT_GIT_REPOSITORY)
+        set(GIT_INFO "-unknown")
     else()
-        set(GIT_INFO "-${GIT_REV}")
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD --
+            OUTPUT_VARIABLE GIT_REV OUTPUT_STRIP_TRAILING_WHITESPACE
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        )
+
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} diff-index --quiet HEAD --
+            RESULT_VARIABLE GIT_DIRTY
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        )
+
+        if(GIT_DIRTY)
+            set(GIT_INFO "-${GIT_REV}-dirty")
+        else()
+            set(GIT_INFO "-${GIT_REV}")
+        endif()
     endif()
 
 else()
     message(WARNING "git missing -- unable to check libladeRF version.")
+    unset(NOT_GIT_REPOSITORY)
     unset(GIT_REV)
     unset(GIT_DIRTY)
 endif()
@@ -79,8 +96,12 @@ endif()
 ################################################################################
 set(VERSION_INFO_BASE "${VER_MAJ}.${VER_MIN}.${VER_PAT}")
 
+# Force the version suffix.  Used for automated export builds.
+if(VERSION_INFO_OVERRIDE)
+    set(VERSION_INFO "${VERSION_INFO_BASE}-${VERSION_INFO_OVERRIDE}")
+
 # Intra-release builds
-if("${VERSION_INFO_EXTRA}" STREQUAL "git")
+elseif("${VERSION_INFO_EXTRA}" STREQUAL "git")
     set(VERSION_INFO "${VERSION_INFO_BASE}-git${GIT_INFO}")
 
 # Versioned releases
