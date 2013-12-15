de115d52fb1b28bf026e498b6c14926309957d8b
brent <bjosef@gmail.com>
2013-12-15 17:11:30 -0500

    Fix gain/phase address order

--- firmware_common/bladeRF.h
+++ firmware_common/bladeRF.h
@@ -128,10 +128,10 @@ struct bladeRF_sector {
 #define DATA_BUF_SZ     (1024*4)
 
 #define UART_PKT_DEV_GPIO_ADDR          0
-#define UART_PKT_DEV_RX_PHASE_ADDR      4
-#define UART_PKT_DEV_RX_GAIN_ADDR       6
-#define UART_PKT_DEV_TX_PHASE_ADDR      8
-#define UART_PKT_DEV_TX_GAIN_ADDR       10
+#define UART_PKT_DEV_RX_GAIN_ADDR       4
+#define UART_PKT_DEV_RX_PHASE_ADDR      6
+#define UART_PKT_DEV_TX_GAIN_ADDR       8
+#define UART_PKT_DEV_TX_PHASE_ADDR      10
 #define UART_PKT_DEV_FGPA_VERSION_ID    12
 
 struct uart_pkt {
