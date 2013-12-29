ead7b9dc8c5af53473b3c7e3cdc8bdd9d5271318
Robert Ghilduta <robert.ghilduta@nuand.com>
2013-12-28 22:25:22 -0500

    hdl: add RX and TX meta FIFOs
    These FIFOs will be used to store timestamp, modulation, and coding data
    on a per packet basis. The meta header format is described in the
    following commit.

---
+++ hdl/fpga/ip/altera/rx_meta_fifo/rx_meta_fifo_inst.vhd
@@ -0,0 +1,15 @@
+rx_meta_fifo_inst : rx_meta_fifo PORT MAP (
+		aclr	 => aclr_sig,
+		data	 => data_sig,
+		rdclk	 => rdclk_sig,
+		rdreq	 => rdreq_sig,
+		wrclk	 => wrclk_sig,
+		wrreq	 => wrreq_sig,
+		q	 => q_sig,
+		rdempty	 => rdempty_sig,
+		rdfull	 => rdfull_sig,
+		rdusedw	 => rdusedw_sig,
+		wrempty	 => wrempty_sig,
+		wrfull	 => wrfull_sig,
+		wrusedw	 => wrusedw_sig
+	);
