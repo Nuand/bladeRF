1ad6491b651d138d393eb9e7abdb11de47a001ef
lazydodo <github@lazydodo.com>
2014-01-22 19:32:24 -0700

    bw test update to 32 bit.

--- host/utilities/bladeRF-cli/src/cmd/benchmark.c
+++ host/utilities/bladeRF-cli/src/cmd/benchmark.c
@@ -1,22 +1,22 @@
 /*
- * This file is part of the bladeRF project
- *
- * Copyright (C) 2013 Nuand LLC
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
+* This file is part of the bladeRF project
+*
+* Copyright (C) 2013 Nuand LLC
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 #include "cmd.h"
 #include "version.h"
 #include <stdio.h>
@@ -26,63 +26,53 @@ typedef struct
     int Count;
     int Blocks;
     int MissedSamples;
+    uint64_t ProcessedSamples;
     int LastI;
     int LastQ;
     bool First;
     bool FirstBlock;
 } Bench_data_t;
 
-static void *bench_callback(struct bladerf *dev,
-                         struct bladerf_stream *stream,
-                         struct bladerf_metadata *meta,
-                         void *samples,
-                         size_t num_samples,
-                         void *user_data)
+static void *bench_callback32(struct bladerf *dev,                         
+struct bladerf_stream *stream,
+struct bladerf_metadata *meta,
+    void *samples,
+    size_t num_samples,
+    void *user_data)
 {
-    unsigned int i;
-    uint16_t *Data = (uint16_t*) samples;
     Bench_data_t *CBData = (Bench_data_t*) user_data;
-    int Start=0;
-    
-    if(CBData->FirstBlock) //First block will have left over data in it.. lets never look at it..
+    uint32_t *Data = (uint32_t*)samples;
+    uint32_t Diff = Data[num_samples-1]-Data[0];
+    int ExtraSamples ;
+
+    if (CBData->FirstBlock)       //First block might still have some of the old counters in the block.. ignore it..
     {
-        CBData->FirstBlock =false;
+        CBData->FirstBlock=false;
         return samples;
     }
-
-    if (CBData->First)
-    {
-        Start=1;
-        CBData->LastI = Data[0];
-        CBData->LastQ = Data[1];
-        CBData->First = false;
-    }
     
-    CBData->Count--;
-
-    for (i=Start; i<num_samples; i++)
+    ExtraSamples = Diff - num_samples;
+    if (ExtraSamples >0)
     {
-        int NewValue =(CBData->LastI+1) % 2048;
-        if (NewValue != Data[i*2])
-        {
-            printf("Block %d Offset = %d Skip = %d\n",CBData->Blocks - CBData->Count,i,  (2048+Data[i*2]-NewValue) % 2048);
-            CBData->MissedSamples++;
-        }
-        CBData->LastI = Data[i*2];
+        CBData->MissedSamples +=ExtraSamples;
     }
-
+    CBData->Count--;
+    CBData->ProcessedSamples+=num_samples;
     if (CBData->Count)
     {
         return samples;
     }
     else
+    {
         return NULL;
+    }
 }
+
 
 int cmd_benchmark(struct cli_state *state, int argc, char **argv)
 {
     int status;
-    
+
     bool fpga_loaded = false;
 
     /* Exit cleanly if no device is attached */
@@ -110,22 +100,34 @@ int cmd_benchmark(struct cli_state *state, int argc, char **argv)
 
         printf("Starting bandwidth benchmark.\n");
         bladerf_config_gpio_read(state->dev,  &GpioValue);
-        bladerf_config_gpio_write(state->dev, GpioValue | (1<<8) );//Enable counting
+        bladerf_config_gpio_write(state->dev, GpioValue | (2<<8) );//Enable counting
         bladerf_enable_module(state->dev, BLADERF_MODULE_RX, true);
         for (i=1; i<=40; i++)
         {
+            int BlockSize ;
             BW = (unsigned int) (i*1E6);
+            BlockSize = (BW/10) & (~0xffff); //100ms blocks with 64k boundary
+            if (BlockSize > 1024*1024)
+                BlockSize = 1024*1024;
             bladerf_set_sample_rate(state->dev, BLADERF_MODULE_RX, (unsigned int)BW, &Actual); 
             Data.First = true;
             Data.FirstBlock = true;
             Data.LastI = 0;
             Data.LastQ = 0;
-            Data.Count = Data.Blocks =  (int) (((((double)BW) / (1024*1024))*5)+1); //5sec or next closest..
+            Data.Count = Data.Blocks =  (int) (((((double)BW) / (BlockSize))*15)+2); //15sec or next closest..
+            //printf("Speed : %dMhz Blocks = %d BlockSize = %d\n",i,Data.Count,BlockSize);
             Data.MissedSamples = 0;
-            bladerf_init_stream(&Stream, state->dev, bench_callback, &Buffers, 64, BLADERF_FORMAT_SC16_Q12, (size_t)(1024*1024) ,64,&Data);
-            bladerf_stream(Stream , BLADERF_MODULE_RX);
-            bladerf_deinit_stream(Stream);
-            printf("Speed : %dMhz missed = %d \n",(int)(BW/(1E6)) ,Data.MissedSamples);
+            Data.ProcessedSamples=0;
+            if (bladerf_init_stream(&Stream, state->dev, bench_callback32, &Buffers, 64, BLADERF_FORMAT_SC16_Q12, (size_t)(BlockSize), 64,&Data)==0)
+            {
+                bladerf_stream(Stream , BLADERF_MODULE_RX);
+                bladerf_deinit_stream(Stream);
+                printf("Speed : %dMhz processed = %d, missed = %d \n",(int)(BW/(1E6)) ,(int)Data.ProcessedSamples, Data.MissedSamples);
+            }
+            else
+            {
+                printf("Boo %dMhz\n",(int)(BW/1E6));
+            }
         }
         bladerf_config_gpio_write(state->dev, GpioValue  );//Restore to original setting
         bladerf_enable_module(state->dev, BLADERF_MODULE_RX, false);
