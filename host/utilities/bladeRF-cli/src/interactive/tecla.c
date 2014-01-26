638f07ba9fdee7b8a56220aa1dee97eaf982d089
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-01-25 23:22:15 -0500

    bladeRF-cli: Added filename completion support for 'file=' parameters

--- host/utilities/bladeRF-cli/src/interactive/tecla.c
+++ host/utilities/bladeRF-cli/src/interactive/tecla.c
@@ -25,6 +25,48 @@
 #include "interactive_impl.h"
 
 static GetLine *gl = NULL;
+WordCompletion *tab_complete = NULL;
+
+/* This tab completion is simply to workaround the fact that we throw tecla
+ * off by trying to complete filenames in our "file=" rx/tx command parameters.
+ *
+ * In the future, we could additional command/parameter completions here...
+ */
+int tab_completion(WordCompletion *cpl, void *data, const char *line, int w_end)
+{
+    static const char file_param[] = "file=";
+    char *param = strstr(line, file_param);
+    int ret;
+
+    if (param != NULL) {
+        /* [0] up to "file=" */
+        const size_t param_off = param - line;
+
+        /* Length of everything after "file=" */
+        const size_t remainder = strlen(param) - strlen(file_param);
+
+        char *modified_line;
+
+        modified_line = calloc(strlen(line) - strlen(file_param) + 1, 1);
+        if (modified_line != NULL) {
+            strncpy(modified_line, line, param_off);
+            strncpy(modified_line + strlen(modified_line),
+                    line + param_off + strlen(file_param),
+                    remainder);
+
+            ret = cpl_file_completions(cpl, data, modified_line,
+                                       strlen(modified_line));
+            free(modified_line);
+
+            return ret;
+        } else {
+            cpl_record_error(cpl, "calloc() failure");
+            return 1;
+        }
+    }
+
+    return cpl_file_completions(cpl, data, line, w_end);
+}
 
 int interactive_init()
 {
@@ -46,6 +88,14 @@ int interactive_init()
             fprintf(stderr, SIGHANLDER_FAILED);
         }
 
+        tab_complete = new_WordCompletion();
+        if (tab_complete != NULL) {
+            gl_customize_completion(gl, NULL, tab_completion);
+        } else {
+            /* Non-fatal - complain and carry on */
+            fprintf(stderr, "Failed to initialize tab-completion.\n");
+        }
+
         status = 0;
     }
 
@@ -57,6 +107,10 @@ void interactive_deinit()
     if (gl) {
         del_GetLine(gl);
         gl = NULL;
+    }
+
+    if (tab_complete) {
+        del_WordCompletion(tab_complete);
     }
 }
 
