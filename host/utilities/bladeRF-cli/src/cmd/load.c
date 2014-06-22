4dc24fb0509dfed923649020d5edb691911c64bc
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-06-22 18:14:49 -0400

    bladeRF-cli: Added 'load cal[ibration] <file>' support.

--- host/utilities/bladeRF-cli/src/cmd/load.c
+++ host/utilities/bladeRF-cli/src/cmd/load.c
@@ -22,65 +22,14 @@
 #include "cmd.h"
 #include "input.h"
 
-static int load_fpga(struct cli_state *s, char *file)
-{
-    char *expanded_path;
-    int cmd_status = 0;
-    int lib_status;
-
-    if ((expanded_path = input_expand_path(file)) == NULL) {
-        cli_err(s, "Unable to expand FPGA file path: \"%s\"", file);
-        cmd_status = CLI_RET_INVPARAM;
-    } else {
-        printf("Loading fpga from %s...\n", expanded_path);
-        lib_status = bladerf_load_fpga(s->dev, expanded_path);
-
-        if (lib_status < 0) {
-            s->last_lib_error = lib_status;
-            cmd_status = CLI_RET_LIBBLADERF;
-        } else {
-            printf("Done.\n");
-        }
-
-        free(expanded_path);
-    }
-
-    return cmd_status;
-}
-
-static int load_fx3(struct cli_state *s, char *file)
-{
-    char *expanded_path;
-    int cmd_status = 0;
-    int lib_status;
-
-    if ((expanded_path = input_expand_path(file)) == NULL) {
-        cli_err(s, "Unable to expand firmware file path: \"%s\"", file);
-        cmd_status = CLI_RET_INVPARAM;
-    } else {
-        printf("Flashing firmware from %s...\n", expanded_path);
-        lib_status = bladerf_flash_firmware(s->dev, expanded_path);
-
-        if (lib_status < 0) {
-            s->last_lib_error = lib_status;
-            cmd_status = CLI_RET_LIBBLADERF;
-        } else {
-            printf("Done. Cycle power on the device.\n");
-        }
-
-        free(expanded_path);
-    }
-
-    return cmd_status;
-}
-
 int cmd_load(struct cli_state *state, int argc, char **argv)
 {
     /* Valid commands:
         load fpga <filename>
         load fx3 <filename>
+        load cal[ibration] <filename>
     */
-    int rv = CLI_RET_OK ;
+    int rv = CLI_RET_OK;
 
     if (!cli_device_is_opened(state)) {
         return CLI_RET_NODEV;
@@ -89,15 +38,51 @@ int cmd_load(struct cli_state *state, int argc, char **argv)
     }
 
     if ( argc == 3 ) {
-        if( strcasecmp( argv[1], "fpga" ) == 0 ) {
-            rv = load_fpga(state, argv[2]);
-        } else if( strcasecmp( argv[1], "fx3" ) == 0 ) {
-            rv = load_fx3(state, argv[2]);
+        int lib_status = 0;
+        struct bladerf *dev = state->dev;
+        char *expanded_path = input_expand_path(argv[2]);
+
+        if (expanded_path == NULL) {
+            cli_err(state, "Unable to expand file path: \"%s\"", argv[2]);
+            return CLI_RET_INVPARAM;
+        }
+
+        if (!strcasecmp(argv[1], "fpga")) {
+
+            printf("Loading fpga from %s...\n", expanded_path);
+            lib_status = bladerf_load_fpga(dev, expanded_path);
+            if (lib_status == 0) {
+                printf("Done.\n");
+            }
+
+        } else if (!strcasecmp(argv[1], "fx3")) {
+
+            printf("Flashing firmware from %s...\n", expanded_path);
+            lib_status = bladerf_flash_firmware(dev, expanded_path);
+            if (lib_status == 0) {
+                printf("Done. Cycle power on the device.\n");
+            }
+
+        } else if (!strcasecmp(argv[1], "cal") ||
+                   !strcasecmp(argv[1], "calibration")) {
+
+            printf("Loading calibration table from %s...\n", expanded_path);
+            lib_status = bladerf_load_calibration_table(dev, expanded_path);
+            if (lib_status == 0) {
+                printf("Done.");
+            }
+
         } else {
-            cli_err(state, argv[0],
-                    "\"%s\" is not a valid programming target\n", argv[1]) ;
+            cli_err(state, argv[0], "Invalid type: %s\n", argv[1]);
             rv = CLI_RET_INVPARAM;
         }
+
+        free(expanded_path);
+        if (lib_status < 0) {
+            state->last_lib_error = lib_status;
+            rv = CLI_RET_LIBBLADERF;
+        }
+
     } else {
         rv = CLI_RET_NARGS;
     }
