5758109598025fc4c01f6d724a6ec6d400414e25
Chris Wiener <cwiener@crlabs.com>
2014-07-30 07:43:07 -0400

    Add support for selecting XB-200 filter [issue 288]

--- host/utilities/bladeRF-cli/src/cmd/xb.c
+++ host/utilities/bladeRF-cli/src/cmd/xb.c
@@ -31,17 +31,56 @@ int cmd_xb(struct cli_state *state, int argc, char **argv)
     }
 
     if (argc >= 2) {
-        if (argc >= 3 && !strcmp(argv[1], "enable")) {
-            if (!strcmp(argv[2], "200")) {
-                printf("  Enabling transverter board\n");
-                status = bladerf_expansion_attach(state->dev, BLADERF_XB_200);
-                if (status != 0) {
-                    state->last_lib_error = status;
-                    return CLI_RET_LIBBLADERF;
-                }
-                printf("  Transverter board successfully enabled\n");
+      if (argc >= 3) {
+        // xb enable 200
+        if (!strcmp(argv[1], "enable")) {
+          if (!strcmp(argv[2], "200")) {
+            printf(" Enabling transverter board\n");
+            status = bladerf_expansion_attach(state->dev, BLADERF_XB_200);
+            if (status != 0) {
+              state->last_lib_error = status;
+              return CLI_RET_LIBBLADERF;
             }
+            printf(" Transverter board successfully enabled\n");
+          }
         }
+        // xb filter 200 xx yy  - where xx is tx or rx and yy is the filter to be selected
+        else if ( (5 == argc) && !strcmp(argv[1], "filter") && !strcmp(argv[2], "200") ) {
+          bladerf_xb200_filter filter;
+          bladerf_module mod;
+
+          // get module to set filter for
+          if (!strcmp(argv[3], "rx"))
+            mod = BLADERF_MODULE_RX;
+          else if (!strcmp(argv[3], "tx"))
+            mod = BLADERF_MODULE_TX;
+          else return CLI_RET_INVPARAM;
+
+          // get which filter to set
+          if (!strcmp(argv[4], "50"))
+            filter = BLADERF_XB200_50M;
+          else if (!strcmp(argv[4], "144"))
+            filter = BLADERF_XB200_144M;
+          else if (!strcmp(argv[4], "222"))
+            filter = BLADERF_XB200_222M;
+          else if (!strcmp(argv[4], "custom"))
+            filter = BLADERF_XB200_CUSTOM;
+          else if (!strcmp(argv[4], "auto_1db"))
+            filter = BLADERF_XB200_AUTO_1DB;
+          else if (!strcmp(argv[4], "auto_3db"))
+            filter = BLADERF_XB200_AUTO_3DB;
+          else return CLI_RET_INVPARAM;
+
+          status = bladerf_xb200_set_filterbank(state->dev, mod, filter);
+
+          if (status != 0) {
+            state->last_lib_error = status;
+            return CLI_RET_LIBBLADERF;
+          }
+          printf(" Transverter board %s filter successfully set to %s\n", argv[3], argv[4]);
+        }
+      }
     }
+
     return status;
 }
