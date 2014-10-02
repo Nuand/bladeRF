Nuand/bladeRF

commit 62a0ab5ee8d9cb5dbe4d1771f614db15352d7f25

cmake: Skip libc & libusb version checks when cross-compiling
These checks break cross-compilation, as they require code to be
run on the host machine.

host/cmake/modules/FindLibUSB.cmake
@@ -120,7 +120,7 @@ if(LIBUSB_FOUND)
     check_include_file("{LIBUSB_HEADER_FILE}" LIBUSB_FOUND)
 endif(LIBUSB_FOUND)
 
-if(LIBUSB_FOUND)
+if(LIBUSB_FOUND AND NOT CMAKE_CROSSCOMPILING)
 
     # Introduced in v1.0.9 - these functions are absolutely required
     check_library_exists("${usb_LIBRARY}" libusb_error_name "" LIBUSB_HAVE_ERROR_NAME)

host/CMakeLists.txt
@@ -119,10 +119,13 @@ endif(MSVC)
 if(${CMAKE_SYSTEM_NAME} MATCHES "Linux" OR ${CMAKE_SYSTEM_NAME} MATCHES "BSD")
     set(BLADERF_OS_LINUX 1)
 
-    try_run(LIBC_VERSION_RUN_RESULT LIBC_VERSION_COMPILE_RESULT
-            ${CMAKE_CURRENT_BINARY_DIR}/cmake/helpers
-            ${CMAKE_CURRENT_SOURCE_DIR}/cmake/helpers/libc_version.c
-            RUN_OUTPUT_VARIABLE LIBC_VERSION)
+    if(NOT CMAKE_CROSSCOMPILING)
+        message(STATUS "Checking libc version...")
+        try_run(LIBC_VERSION_RUN_RESULT LIBC_VERSION_COMPILE_RESULT
+                ${CMAKE_CURRENT_BINARY_DIR}/cmake/helpers
+                ${CMAKE_CURRENT_SOURCE_DIR}/cmake/helpers/libc_version.c
+                RUN_OUTPUT_VARIABLE LIBC_VERSION)
+    endif(NOT CMAKE_CROSSCOMPILING)
 
     if(LIBC_VERSION)
         message(STATUS "libc version: ${LIBC_VERSION}")

commit a8d6065e616b0d9d25c9d53cb172c7277e450b6b

cmake: Fixed if()/endif() mismatch
This was mistake was introduced in 62a0ab5e.

host/cmake/modules/FindLibUSB.cmake
@@ -148,4 +148,4 @@ if(LIBUSB_FOUND AND NOT CMAKE_CROSSCOMPILING)
     check_library_exists("${usb_LIBRARY}" libusb_handle_events_timeout "" LIBUSB_HAVE_HANDLE_EVENTS_TIMEOUT)
     check_library_exists("${usb_LIBRARY}" libusb_handle_events_timeout_completed "" LIBUSB_HAVE_HANDLE_EVENTS_TIMEOUT_COMPLETED)
 
-endif(LIBUSB_FOUND)
+endif(LIBUSB_FOUND AND NOT CMAKE_CROSSCOMPILING)