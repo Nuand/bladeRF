da19ed24d0dfd26373d374503a208528517adf04
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-10-02 23:31:44 -0400

    libbladeRF: Removed SC16_Q12 reverse compat macro
    This has been scheduled for removal since RC1 and RC2.

--- host/libraries/libbladeRF/include/libbladeRF.h
+++ host/libraries/libbladeRF/include/libbladeRF.h
@@ -1176,15 +1176,6 @@ typedef enum {
     BLADERF_FORMAT_SC16_Q11,
 } bladerf_format;
 
-/**
- * Reverse compatibility for the sample format misnomer fix
- *
- * @warning This is scheduled to be removed in the future.
- */
-#define BLADERF_FORMAT_SC16_Q12 BLADERF_FORMAT_SC16_Q11
-
-
-
 /*
  * Metadata status bits
  */
