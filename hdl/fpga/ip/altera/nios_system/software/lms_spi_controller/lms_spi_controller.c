47a971a32cb6568079d39dcc4dd31764e107a10c
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-10-22 16:23:28 -0400

    hdl, libbladeRF: Bumped FPGA version to v0.1.1 for SPI timing fix

--- hdl/fpga/ip/altera/nios_system/software/lms_spi_controller/lms_spi_controller.c
+++ hdl/fpga/ip/altera/nios_system/software/lms_spi_controller/lms_spi_controller.c
@@ -47,7 +47,7 @@
 #define FPGA_VERSION_ID         0x7777
 #define FPGA_VERSION_MAJOR      0
 #define FPGA_VERSION_MINOR      1
-#define FPGA_VERSION_PATCH      0
+#define FPGA_VERSION_PATCH      1
 #define FPGA_VERSION            (FPGA_VERSION_MAJOR | (FPGA_VERSION_MINOR << 8) | (FPGA_VERSION_PATCH << 16))
 
 #define TIME_TAMER              TIME_TAMER_0_BASE
