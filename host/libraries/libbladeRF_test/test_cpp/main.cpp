c6512aabcd408d19476392d4c87521fb48371e71
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-11-13 19:15:06 -0500

    libbladeRF: Added C/C++ build tests
    These programs are just intended to ensure a simple C/C++ programs using
    libbladeRF builds (without requiring anything from host/common).

---
+++ host/libraries/libbladeRF_test/test_cpp/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the bladeRF project:
+ *   http://www.github.com/nuand/bladeRF
+ *
+ * Copyright (C) 2014 Nuand LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This program is intended to verify that C++ programs build against
+ * libbladeRF without any unintended dependencies.
+ */
+#include <iostream>
+#include <libbladeRF.h>
+
+int main(int argc, char *argv[])
+{
+    struct bladerf_version version;
+    bladerf_version(&version);
+    std::cout << "libbladeRF " << version.describe << std::endl;
+    return 0;
+}
