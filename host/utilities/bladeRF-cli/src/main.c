13668758b37f36a3eaead523a086fa028ad52ca1
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-12-18 16:56:58 -0500

    bladeRF-cli: Inform user if bootloader devices are present
    Before attempting to open any devices when going into interactive mode,
    running a script, or executing "-e" arguments, bladeRF-cli now checks
    for any FX3-based devices in bootloader mode, and alerts the user
    to their presence, recommending that he or she see 'help recover'
    for more information.
    
    Previously, a libbladeRF log_info() message was used to alert users
    to the presence of FX3-based devices enumerating in bootloader mode.
    This was removed to alleviate noise and because it was buried in
    backend-specific code.
    
    Given that the bladeRF-cli is intended to be the "go to" program for
    debugging the state of a bladeRF device, this alert is more
    appropriately located in this program.

--- host/utilities/bladeRF-cli/src/main.c
+++ host/utilities/bladeRF-cli/src/main.c
@@ -389,6 +389,27 @@ static int load_fpga(struct rc_config *rc, struct cli_state *state, int status)
     return status;
 }
 
+void check_for_bootloader_devs()
+{
+    int num_devs;
+    struct bladerf_devinfo *list;
+
+    num_devs = bladerf_get_bootloader_list(&list);
+
+    if (num_devs <= 0) {
+        if (num_devs != BLADERF_ERR_NODEV) {
+            fprintf(stderr, "Error: failed to check for bootloader devices.\n");
+        }
+
+        return;
+    }
+
+    bladerf_free_device_list(list);
+    printf("NOTE: One or more FX3-based devices operating in bootloader mode\n"
+           "      were detected. Run 'help recover' to view information about\n"
+           "      downloading firmware to the device(s).\n\n");
+}
+
 int main(int argc, char *argv[])
 {
     int status = 0;
@@ -441,6 +462,8 @@ int main(int argc, char *argv[])
     }
 
     if (!exit_immediately) {
+        check_for_bootloader_devs();
+
         /* Conditionally performed items, depending on runtime config */
         status = open_device(&rc, state, status);
         if (status) {
