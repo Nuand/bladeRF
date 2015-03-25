485612caabe7063a5856ad2398290a923d8bbd6a
Jon Szymaniak <jon.szymaniak@gmail.com>
2015-03-24 23:25:46 -0400

    host/misc/scripts: Added MATLAB scripts to load/save samples

---
+++ host/misc/matlab/load_sc16q11.m
@@ -0,0 +1,28 @@
+function [ signal, signal_i, signal_q ] = load_sc16q11(filename)
+% LOAD_SC16Q11 Read a normalized complex signal from a binary file in the 
+%   bladeRF "SC16 Q11" format.
+%
+%   [SIGNAL, SIGNAL_I, SIGNAL_Q] = load_sc16q11(FILENAME)
+%
+%   FILENAME is the source filename.
+% 
+%   SIGNAL is a complex signal with the real and imaginary components
+%   within the range [-1.0, 1.0).
+%
+%   SIGNAL_I and SIGNAL_Q are optional return values which contain the 
+%   real and imaginary components of SIGNAL as separate vectors.
+%
+    [f, err_msg] = fopen(filename, 'r', 'ieee-le');
+    if f ~= -1
+        data = fread(f, Inf, 'int16');
+        
+        signal_i = data(1:2:end, :) ./ 2048.0;
+        signal_q = data(2:2:end, :) ./ 2048.0;
+        
+        signal = signal_i + 1i .* signal_q;
+        
+        fclose(f);
+    else
+        error(err_msg);
+    end
+end
---
+++ host/misc/matlab/save_sc16q11.m
@@ -0,0 +1,40 @@
+function [ret] = save_sc16q11(filename, signal)
+% SAVE_SC16Q11 Write a normalized complex signal to a binary file in the 
+%   bladeRF "SC16 Q11" format.
+%
+%   [RET] = save_sc16q11(FILENAME, SIGNAL)
+%
+%   RET is 1 on success, and 0 on failure.
+%
+%   FILENAME is the target filename. The file will be overwritten if it
+%   already exists. The file data is written in little-endian format.
+% 
+%   SIGNAL is a complex signal with the real and imaginary components
+%   within the range [-1.0, 1.0). Note that the 1.0 upper bound is
+%   exclusive to avoid a t wrap-around in the target two's complement
+%   value. You may want to scale your input signal by some near-1.0 value
+%   to achieve this, if it cannot be guaranteed otherwise.
+
+    [f, err_msg] = fopen(filename, 'w', 'ieee-le');
+    if f ~= -1
+        sig_i = round(real(signal) .* 2048.0);
+        sig_q = round(imag(signal) .* 2048.0);
+        
+        assert(length(sig_i) == length(sig_q));
+        sig_len = 2 * length(sig_i);
+        
+        sig_out(1:2:sig_len) = sig_i;
+        sig_out(2:2:sig_len) = sig_q;
+        
+        count = fwrite(f, sig_out, 'int16');
+        fclose(f);
+        
+        if count == sig_len;
+            ret = 1;
+        else
+        end
+    else
+        error(err_msg);
+        ret = 0;
+    end
+end
