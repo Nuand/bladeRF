e72d989e370a1d1f30c813e1db303ba76483b5f5
Jon Szymaniak <jon.szymaniak@gmail.com>
2015-05-28 10:21:43 -0400

    libbladeRF_test: Added both RX/TX to scheduled retune test

--- host/libraries/libbladeRF_test/test_scheduled_retune/src/main.c
+++ host/libraries/libbladeRF_test/test_scheduled_retune/src/main.c
@@ -27,9 +27,11 @@
  * This program exercises the scheduled retune functionality added in
  * FPGA v0.2.0.
  *
- * It transmits a tone for 1ms every 10 ms.  During the
+ * When configured for TX, it transmits a tone for 1ms every 10 ms.  During the
  * "off time" the device is scheduled to retune to a different frequency,
  * per the list of frequencies provided to this program.
+ *
+ * When configured for RX, this program reads samples to a samples.bin file.
  *
  * The "hop set" file should be a newline-delimited list of frequencies.
  * The M or G suffixes may be used, as well as scientific notation.
@@ -53,44 +55,60 @@
 #define TS_INC      20000
 #define RETUNE_INC  (NUM_SAMPLES + (TS_INC - NUM_SAMPLES) / 10)
 
-int run_test(struct bladerf *dev, struct hop_set *hops, bool quick_tune)
+int run_test(struct bladerf *dev, bladerf_module module,
+             struct hop_set *hops, bool quick_tune)
+
 {
     int status = -1;
     int16_t *samples = NULL;
     struct bladerf_metadata meta;
     unsigned int i;
     uint64_t retune_ts;
+    FILE *out = NULL;
 
     memset(&meta, 0, sizeof(meta));
 
-    samples = malloc(2 * NUM_SAMPLES * sizeof(samples[0]));
+    samples = calloc(2 * NUM_SAMPLES, sizeof(samples[0]));
     if (samples == NULL) {
         perror("malloc");
         return -1;
     }
 
-    /* Set IQ values to ~ sqrt(2)/2 */
-    for (i = 0; i < (2 * NUM_SAMPLES); i += 2) {
-        samples[i] = samples[i+1] = 1448;
+    if (module == BLADERF_MODULE_TX) {
+        /* Set IQ values to ~ sqrt(2)/2 */
+        for (i = 0; i < (2 * NUM_SAMPLES); i += 2) {
+            samples[i] = samples[i+1] = 1448;
+        }
+    } else {
+        out = fopen("samples.bin", "wb");
+        if (out == NULL) {
+            perror("fopen");
+            status = -1;
+            goto out;
+        }
     }
 
-    status = bladerf_get_timestamp(dev, BLADERF_MODULE_TX, &meta.timestamp);
+    status = bladerf_get_timestamp(dev, module, &meta.timestamp);
     if (status != 0) {
-        fprintf(stderr, "Failed to get initial TX timestamp: %s\n",
+        fprintf(stderr, "Failed to get initial timestamp: %s\n",
                 bladerf_strerror(status));
         goto out;
     }
 
     /* Add some initial startup delay */
     meta.timestamp += (SAMPLE_RATE / 50);
-    meta.flags = BLADERF_META_FLAG_TX_BURST_START |
-                 BLADERF_META_FLAG_TX_BURST_END;
+
+    if (module == BLADERF_MODULE_TX) {
+        meta.flags = BLADERF_META_FLAG_TX_BURST_START |
+                     BLADERF_META_FLAG_TX_BURST_END;
+    } else {
+        meta.flags = 0;
+    }
 
     retune_ts = meta.timestamp + RETUNE_INC;
 
     /* Set initial frequency */
-    status = bladerf_set_frequency(dev, BLADERF_MODULE_TX,
-                                   hop_set_next(hops, NULL));
+    status = bladerf_set_frequency(dev, module, hop_set_next(hops, NULL));
     if (status != 0) {
         fprintf(stderr, "Failed to set initial frequency: %s\n",
                 bladerf_strerror(status));
@@ -98,15 +116,33 @@ int run_test(struct bladerf *dev, struct hop_set *hops, bool quick_tune)
     }
 
     for (i = 0; i < ITERATIONS; i++) {
-        status = bladerf_sync_tx(dev, samples, NUM_SAMPLES, &meta, TIMEOUT_MS);
-        if (status != 0) {
-            fprintf(stderr, "Failed to TX @ iteration %u: %s\n",
-                    i + 1, bladerf_strerror(status));
-            goto out;
+        if (module == BLADERF_MODULE_TX) {
+            status = bladerf_sync_tx(dev, samples, NUM_SAMPLES, &meta, TIMEOUT_MS);
+            if (status != 0) {
+                fprintf(stderr, "Failed to TX @ iteration %u: %s\n",
+                        i + 1, bladerf_strerror(status));
+                goto out;
+            }
         } else {
-            /* Update timestamp for next transmission */
-            meta.timestamp += TS_INC;
+            size_t n_written;
+
+            status = bladerf_sync_rx(dev, samples, NUM_SAMPLES, &meta, TIMEOUT_MS);
+            if (status != 0) {
+                fprintf(stderr, "Failed to RX @ iteration %u: %s\n",
+                        i + 1, bladerf_strerror(status));
+                goto out;
+            }
+
+            n_written = fwrite(samples, 2 * sizeof(int16_t), NUM_SAMPLES, out);
+            if (n_written != NUM_SAMPLES) {
+                fprintf(stderr, "File write of samples failed.\n");
+                status = -1;
+                goto out;
+            }
         }
+
+        /* Update timestamp for next transmission */
+        meta.timestamp += TS_INC;
 
         if (quick_tune) {
             struct hop_params p;
@@ -137,6 +173,10 @@ int run_test(struct bladerf *dev, struct hop_set *hops, bool quick_tune)
     }
 
 out:
+    if (out != NULL) {
+        fclose(out);
+    }
+
     free(samples);
     return status;
 }
@@ -148,23 +188,36 @@ int main(int argc, char *argv[])
     const char *dev_str = NULL;
     const char *hop_file = NULL;
     const char *mode = NULL;
+    const char *module_str;
     struct hop_set *hops;
     struct device_config config;
+    bladerf_module module;
     bool quick_tune;
     size_t i;
 
     if (argc < 3) {
-        fprintf(stderr, "Usage: %s [device str] <hop file> <quick/normal>\n",
+        fprintf(stderr, "Usage: %s [device str] <rx/tx> <hop file> <quick/normal>\n",
                 argv[0]);
 
         return 1;
-    } else if (argc >= 4) {
+    } else if (argc >= 5) {
         dev_str = argv[1];
+        module_str = argv[2];
+        hop_file = argv[3];
+        mode = argv[4];
+    } else {
+        module_str = argv[1];
         hop_file = argv[2];
         mode = argv[3];
+    }
+
+    if (!strcasecmp(module_str, "rx")) {
+        module = BLADERF_MODULE_RX;
+    } else if (!strcasecmp(module_str, "tx")) {
+        module = BLADERF_MODULE_TX;
     } else {
-        hop_file = argv[1];
-        mode = argv[2];
+        fprintf(stderr, "Invalid module: %s\n", module_str);
+        return 1;
     }
 
     if (!strcasecmp(mode, "quick")) {
@@ -227,7 +280,7 @@ int main(int argc, char *argv[])
     hops->idx = 0;
     printf("\n");
 
-    status = test_perform_sync_config(dev, BLADERF_MODULE_TX,
+    status = test_perform_sync_config(dev, module,
                                       BLADERF_FORMAT_SC16_Q11_META,
                                       &config, true);
     if (status != 0) {
@@ -237,7 +290,7 @@ int main(int argc, char *argv[])
     printf("Running...\n");
     fflush(stdout);
 
-    status = run_test(dev, hops, quick_tune);
+    status = run_test(dev, module, hops, quick_tune);
 
     printf("Done.\n");
     fflush(stdout);
