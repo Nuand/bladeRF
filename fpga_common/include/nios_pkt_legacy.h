88bb372cf546025bd4c7d38542b88706ce1b44fa
Jon Szymaniak <jon.szymaniak@gmail.com>
2015-06-08 18:21:30 -0400

    fpga_common: Cleaned up header for NIOS II legacy packet format
    Removed unused struct and union.
    
    Added summary of the packet format and its response.

--- fpga_common/include/nios_pkt_legacy.h
+++ fpga_common/include/nios_pkt_legacy.h
@@ -20,10 +20,103 @@
  * THE SOFTWARE.
  */
 
-/* This file defines the legacy Host <-> FPGA (NIOS II) packet format */
-
 #ifndef BLADERF_NIOS_PKT_LEGACY_H_
 #define BLADERF_NIOS_PKT_LEGACY_H_
+
+/* This is the original packet format used to issue requests from the
+ * host to the FPGA via the FX3 UART.
+ *
+ * This format remains supported for backwards compatibility, but should no
+ * longer be added to.
+ *
+ * If you're looking to customize the FPGA, consider using
+ * one of the "pkt_AxB" packet formats and handlers, or implementing a new
+ * format and handler.
+ *
+ *                              Request
+ *                      ----------------------
+ *
+ * +================+=========================================================+
+ * |  Byte offset   |                       Description                       |
+ * +================+=========================================================+
+ * |        0       | Magic Value                                             |
+ * +----------------+---------------------------------------------------------+
+ * |        1       | Configuration byte (Note 1)                             |
+ * +----------------+---------------------------------------------------------+
+ * |      2 - 15    | Pairs of 8-bit addr, 8-bit data                         |
+ * +----------------+---------------------------------------------------------+
+ *
+ *
+ *
+ * Note 1: Configuration byte:
+ *
+ * +================+========================+
+ * |      Bit(s)    |         Value          |
+ * +================+========================+
+ * |        7       |   1 = Read operation   |
+ * +----------------+------------------------+
+ * |        6       |   1 = Write operation  |
+ * +----------------+------------------------+
+ * |       5:4      | Device:                |
+ * |                |   00 - Config PIO      |
+ * |                |   01 - LMS register    |
+ * |                |   10 - VCTCXO Trim DAC |
+ * |                |   11 - SI5338 register |
+ * +----------------+------------------------+
+ * |        3       | Unused                 |
+ * +----------------+------------------------+
+ * |       2:0      | Addr/Data pair count   |
+ * |                | (Note 2)               |
+ * +----------------+------------------------+
+ *
+ *
+ * Note 2: "Count" field
+ *
+ * The original intent of this field was to allow multiple register
+ * accesses to be requested at once.
+ *
+ * However, this feature was not leveraged by the host code for the LMS and
+ * SI5338 accesses, so revised legacy packet handler only processes the
+ * first addr/data pair.
+ *
+ * Readback of the time tamer values is the only case where this field
+ * is set to a count greater than 1.
+ *
+ * Although config PIO values are larger than one byte, the host code
+ * accessed these byte by byte through multiple requests.  For example,
+ * 4 accesses would be required to fully read/write the configuration PIO.
+ *
+ * The above inefficiency is the motivation behind adding packet handlers
+ * that can read/write 32 or 64 bits in a single request (e.g., pkt_8x32,
+ * pkt_8x64).
+ *
+ *
+ *
+ *                              Response
+ *                      ----------------------
+ *
+ * The response for the legacy packet is essentially just the device
+ * echoing the request.
+ *
+ * On a read request, the number of requested items will be populated
+ * in bytes 2:15.
+ *
+ * The remaining bytes, or all of bytes 2:15 on a write request, should
+ * be regarded as "undefined" values and not used.
+ *
+ * +================+=========================================================+
+ * |  Byte offset   |                       Description                       |
+ * +================+=========================================================+
+ * |        0       | Magic Value                                             |
+ * +----------------+---------------------------------------------------------+
+ * |        1       | Configuration byte                                      |
+ * +----------------+---------------------------------------------------------+
+ * |      2 - 15    | Pairs of 8-bit addr, 8-bit data                         |
+ * +----------------+---------------------------------------------------------+
+ *
+ */
+
+#define UART_PKT_MAGIC                  'N'
 
 #define UART_PKT_DEV_GPIO_ADDR          0
 #define UART_PKT_DEV_RX_GAIN_ADDR       4
@@ -32,18 +125,11 @@
 #define UART_PKT_DEV_TX_PHASE_ADDR      10
 #define UART_PKT_DEV_FGPA_VERSION_ID    12
 
-struct uart_pkt {
-    unsigned char magic;
-#define UART_PKT_MAGIC          'N'
-
-    //  | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
-    //  |  dir  |  dev  |     cnt       |
-    unsigned char mode;
 #define UART_PKT_MODE_CNT_MASK   0x7
 #define UART_PKT_MODE_CNT_SHIFT  0
-
 #define UART_PKT_MODE_DEV_MASK   0x30
 #define UART_PKT_MODE_DEV_SHIFT  4
+
 #define UART_PKT_DEV_CONFIG      (0<<UART_PKT_MODE_DEV_SHIFT)
 #define UART_PKT_DEV_LMS         (1<<UART_PKT_MODE_DEV_SHIFT)
 #define UART_PKT_DEV_VCTCXO      (2<<UART_PKT_MODE_DEV_SHIFT)
@@ -53,16 +139,10 @@ struct uart_pkt {
 #define UART_PKT_MODE_DIR_SHIFT  6
 #define UART_PKT_MODE_DIR_READ   (2<<UART_PKT_MODE_DIR_SHIFT)
 #define UART_PKT_MODE_DIR_WRITE  (1<<UART_PKT_MODE_DIR_SHIFT)
-};
 
 struct uart_cmd {
-    union {
-        struct {
-            unsigned char addr;
-            unsigned char data;
-        };
-        unsigned short word;
-    };
+    unsigned char addr;
+    unsigned char data;
 };
 
 #endif
