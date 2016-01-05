872759540d830fc2df19161b457b6b8e44f0458c
Jon Szymaniak <jon.szymaniak@nuand.com>
2016-01-05 15:48:48 -0500

    libbladeRF: Updated to v1.5.1

--- host/libraries/libbladeRF/include/libbladeRF.h
+++ host/libraries/libbladeRF/include/libbladeRF.h
@@ -50,7 +50,7 @@
  *
  *  https://github.com/Nuand/bladeRF/blob/master/doc/development/versioning.md
  */
-#define LIBBLADERF_API_VERSION (0x01050000)
+#define LIBBLADERF_API_VERSION (0x01050100)
 
 #ifdef __cplusplus
 extern "C" {
