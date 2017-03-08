49d888d24b237a4b9728a7fed958570765dddeaf
Brian Glod <brian.glod@nuand.com>
2017-03-07 23:56:35 -0500

    bladerf: nios: Genericized makefiles

--- hdl/fpga/platforms/common/bladerf/software/bladeRF_nios/pcsim.mk
+++ hdl/fpga/platforms/common/bladerf/software/bladeRF_nios/pcsim.mk
@@ -1,18 +1,29 @@
-FPGA_COMMON_DIR := ../../../../../../../fpga_common
-INCLUDES := -I $(FPGA_COMMON_DIR)/include -I src
+#------------------------------------------------------------------------------
+#              VARIABLES TO BE DEFINED BY PLATFORM MAKEFILE
+#------------------------------------------------------------------------------
+
+#  - FPGA_COMMON_DIR    : Path to fpga_common
+#  - BLADERF_COMMON_DIR : Path to common bladeRF_nios dir
+#  - NIOS_BUILD_OUTDIR  : Path to place the Nios build products
+
+#------------------------------------------------------------------------------
+#              MAKEFILE TARGETS
+#------------------------------------------------------------------------------
+
+INCLUDES := -I $(FPGA_COMMON_DIR)/include -I $(BLADERF_COMMON_DIR)/src
 
 CFLAGS := -Wall -Wextra -Wno-unused-parameter \
           -O0 -ggdb3 -DBLADERF_NIOS_PC_SIMULATION \
           $(INCLUDES)
 
-SRC := $(wildcard src/*.c) $(FPGA_COMMON_DIR)/src/band_select.c
+SRC := $(wildcard $(BLADERF_COMMON_DIR)/src/*.c) $(FPGA_COMMON_DIR)/src/band_select.c
 
-all: bladeRF_nios.sim
+all: $(NIOS_BUILD_OUTDIR)/bladeRF_nios.sim
 
 bladeRF_nios.sim: $(SRC)
 	$(CC) $(CFLAGS) $^ -o $@
 
 clean:
-	rm -f bladeRF_nios.sim
+	rm -f $(NIOS_BUILD_OUTDIR)/bladeRF_nios.sim
 
 .PHONY: clean
