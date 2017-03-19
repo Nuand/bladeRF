62c397243e1707527d9ab01dfce0000f3abe2edf
Brian Glod <brian.glod@nuand.com>
2017-03-18 22:13:05 -0400

    bladerf: qsys: Broke out the common Qsys parts
    Made a common Qsys system with the NIOS and other common blocks. Used
    this common system as a subsystem to the platform-specific Qsys. Updated
    C header files as necessary.

--- hdl/fpga/platforms/common/bladerf/software/bladeRF_nios/src/devices_inline.h
+++ hdl/fpga/platforms/common/bladerf/software/bladeRF_nios/src/devices_inline.h
@@ -39,50 +39,50 @@
 
 static inline uint32_t control_reg_read(void)
 {
-    return IORD_ALTERA_AVALON_PIO_DATA(CONTROL_BASE);
+    return IORD_ALTERA_AVALON_PIO_DATA(COMMON_SYSTEM_0_CONTROL_BASE);
 }
 
 static inline void control_reg_write(uint32_t value)
 {
-    IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, value);
+    IOWR_ALTERA_AVALON_PIO_DATA(COMMON_SYSTEM_0_CONTROL_BASE, value);
 }
 
 static inline uint32_t expansion_port_read(void)
 {
-    return IORD_ALTERA_AVALON_PIO_DATA(XB_GPIO_BASE);
+    return IORD_ALTERA_AVALON_PIO_DATA(COMMON_SYSTEM_0_XB_GPIO_BASE);
 }
 
 INLINE void expansion_port_write(uint32_t value)
 {
-    IOWR_ALTERA_AVALON_PIO_DATA(XB_GPIO_BASE, value);
+    IOWR_ALTERA_AVALON_PIO_DATA(COMMON_SYSTEM_0_XB_GPIO_BASE, value);
 }
 
 INLINE uint32_t expansion_port_get_direction()
 {
-    return IORD_ALTERA_AVALON_PIO_DATA(XB_GPIO_DIR_BASE);
+    return IORD_ALTERA_AVALON_PIO_DATA(COMMON_SYSTEM_0_XB_GPIO_DIR_BASE);
 }
 
 INLINE void expansion_port_set_direction(uint32_t dir)
 {
-    IOWR_ALTERA_AVALON_PIO_DATA(XB_GPIO_DIR_BASE, dir);
+    IOWR_ALTERA_AVALON_PIO_DATA(COMMON_SYSTEM_0_XB_GPIO_DIR_BASE, dir);
 }
 
 INLINE void time_tamer_reset(bladerf_module m)
 {
     /* A single write is sufficient to clear the timestamp counter */
     if (m == BLADERF_MODULE_RX) {
-        IOWR_8DIRECT(RX_TAMER_BASE, 0, 0);
+        IOWR_8DIRECT(COMMON_SYSTEM_0_RX_TAMER_BASE, 0, 0);
     } else {
-        IOWR_8DIRECT(TX_TAMER_BASE, 0, 0);
+        IOWR_8DIRECT(COMMON_SYSTEM_0_TX_TAMER_BASE, 0, 0);
     }
 }
 
 INLINE void timer_tamer_clear_interrupt(bladerf_module m)
 {
     if (m == BLADERF_MODULE_RX) {
-        IOWR_8DIRECT(RX_TAMER_BASE, 8, 1) ;
+        IOWR_8DIRECT(COMMON_SYSTEM_0_RX_TAMER_BASE, 8, 1) ;
     } else {
-        IOWR_8DIRECT(TX_TAMER_BASE, 8, 1) ;
+        IOWR_8DIRECT(COMMON_SYSTEM_0_TX_TAMER_BASE, 8, 1) ;
     }
 }
 
@@ -90,7 +90,7 @@ INLINE void command_uart_read_request(uint8_t *req) {
     int i, x ;
     uint32_t val ;
     for( x = 0 ; x < 16 ; x+=4 ) {
-        val = IORD_32DIRECT(COMMAND_UART_BASE, x) ;
+        val = IORD_32DIRECT(COMMON_SYSTEM_0_COMMAND_UART_BASE, x) ;
         for( i = 0 ; i < 4 ; i++ ) {
             req[x+i] = val&0xff ;
             val >>= 8 ;
@@ -104,7 +104,7 @@ INLINE void command_uart_write_response(uint8_t *resp) {
     uint32_t val ;
     for( i = 0 ; i < 16 ; i+=4 ) {
         val = ((uint32_t)resp[i+0]) | (((uint32_t)resp[i+1])<<8) | (((uint32_t)resp[i+2])<<16) | (((uint32_t)resp[i+3])<<24) ;
-        IOWR_32DIRECT(COMMAND_UART_BASE, i, val) ;
+        IOWR_32DIRECT(COMMON_SYSTEM_0_COMMAND_UART_BASE, i, val) ;
     }
     return ;
 }
