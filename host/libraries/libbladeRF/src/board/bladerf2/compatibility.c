e6fe2846b969ebd0fa13094d9e44d94c0d711acc
Vanya A. Sergeev <v@sergeev.io>
2017-03-27 20:09:32 -0700

    libbladeRF/bladerf2: update version compatibility table

--- host/libraries/libbladeRF/src/board/bladerf2/compatibility.c
+++ host/libraries/libbladeRF/src/board/bladerf2/compatibility.c
@@ -13,14 +13,14 @@
 
 static const struct compat fw_compat[] = {
     /*   Firmware       requires  >=        FPGA */
-    { VERSION(0, 0, 1),                 VERSION(0, 0, 1) },
+    { VERSION(2, 0, 0),                 VERSION(0, 6, 0) },
 };
 
 const struct version_compat_table bladerf2_fw_compat_table = {fw_compat, ARRAY_SIZE(fw_compat)};
 
 static const struct compat fpga_compat[] = {
     /*    FPGA          requires >=        Firmware */
-    { VERSION(0, 0, 1),                 VERSION(0, 0, 1) },
+    { VERSION(0, 6, 0),                 VERSION(2, 0, 0) },
 };
 
 const struct version_compat_table bladerf2_fpga_compat_table = {fpga_compat, ARRAY_SIZE(fpga_compat)};
