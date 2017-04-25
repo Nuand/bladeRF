001366f266c17b431b2f2c9fc668c060e1ad5e70
Ryan Tucker <ryan.tucker@nuand.com>
2017-04-25 17:16:54 -0400

    hdl/synchronizer: explicitly identify this as a synchronizer

--- hdl/fpga/ip/nuand/synthesis/synchronizer.vhd
+++ hdl/fpga/ip/nuand/synthesis/synchronizer.vhd
@@ -15,19 +15,30 @@ end entity ;
 
 architecture arch of synchronizer is
 
+    attribute ALTERA_ATTRIBUTE  : string;
+    attribute PRESERVE          : boolean;
+
+    signal reg0, reg1   : std_logic;
+
+    attribute ALTERA_ATTRIBUTE of arch  : architecture is "-name SDC_STATEMENT ""set_false_path -to [get_registers {*synchronizer:*|reg0}] """;
+    attribute ALTERA_ATTRIBUTE of reg0  : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
+    attribute ALTERA_ATTRIBUTE of reg1  : signal is "-name SYNCHRONIZER_IDENTIFICATION ""FORCED IF ASYNCHRONOUS""";
+    attribute PRESERVE of reg0          : signal is TRUE;
+    attribute PRESERVE of reg1          : signal is TRUE;
+    attribute PRESERVE of sync          : signal is TRUE;
+
 begin
 
     synchronize : process( clock, reset )
-        variable reg0, reg1 :   std_logic ;
     begin
         if( reset = '1' ) then
             sync <= RESET_LEVEL ;
-            reg0 := RESET_LEVEL ;
-            reg1 := RESET_LEVEL ;
+            reg0 <= RESET_LEVEL ;
+            reg1 <= RESET_LEVEL ;
         elsif( rising_edge( clock ) ) then
             sync <= reg1 ;
-            reg1 := reg0 ;
-            reg0 := async ;
+            reg1 <= reg0 ;
+            reg0 <= async ;
         end if ;
     end process ;
 
