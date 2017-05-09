ca2aef6378963a0d8e202d44b22737ee83049cd9
Brian Glod <brian.glod@nuand.com>
2017-05-09 16:46:29 -0400

    ip: nuand: command_uart: Use .qip to reference HDL
    When a Qsys system is generated, the file dependencies listed in the
    _hw.tcl are all copied into the output directory. Use a .qip instead so
    that only the .qip is copied and the original HDL locations are
    referenced.

--- hdl/fpga/ip/nuand/command_uart/qsys/command_uart_hw.tcl
+++ hdl/fpga/ip/nuand/command_uart/qsys/command_uart_hw.tcl
@@ -40,7 +40,7 @@ add_fileset QUARTUS_SYNTH QUARTUS_SYNTH "" ""
 set_fileset_property QUARTUS_SYNTH TOP_LEVEL command_uart
 set_fileset_property QUARTUS_SYNTH ENABLE_RELATIVE_INCLUDE_PATHS false
 set_fileset_property QUARTUS_SYNTH ENABLE_FILE_OVERWRITE_MODE false
-add_fileset_file ../vhdl/command_uart.vhd VHDL PATH ../vhdl/command_uart.vhd TOP_LEVEL_FILE
+add_fileset_file command_uart.qip OTHER PATH ../command_uart.qip
 
 
 #
