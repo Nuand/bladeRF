ddc015c5fa3208079dfd19680583098dd3d7a13f
Robert Ghilduta <robert.ghilduta@nuand.com>
2017-05-13 01:09:08 -0700

    hdl: Add Automatic Gain Control core
    Automatic Gain Control is essential for a receive chain to drastically increase
    the system's dynamic range. RF frontends, such as the LMS6002D, have static
    ranges of about 6.02dB per bit. In the case of a 12 bit ADC that means 72dB of
    dynamic range. That means that without changing any of the LNA or VGA gains (the
    gain remain unchanged or "static") the RF frontend should be able to "see"
    signals 72dB around the middle of its range which is set by the LNA and VGA
    gains. Although those values seem high, that range is only usable if the
    incoming signal requires an infinitesimally small amount of SNR to demodulate.
    The 802.11a 6Mbps rate requires about 6dB of SNR, so that along with the noise
    figure of the receiver very quickly start taking dB away from the 72dB of
    dynamic range. The only option to be able to listen to a signal, which with its
    SNR requirements falls below the static range, is to start using the RF
    frontend's dnyamic range. The dynamic range of the receiver is adjusted by
    altering the settings of physical amplifiers such as the LNA, and VGAs. For the
    LMS6002D this range goes from about -120dBm to about +4dBm.
    
    At this point an AGC's useful becomes apparent. The AGC quickly and wisely
    adjusts the values of the physical amplifiers to bring the incoming singal
    along with its SNR requirements into the static range of the RF frontend.
    
    The AGC implementation in this commit is specific to 802.11 on the LMS6002D.
    The AGC idles at the higest gain state so as to not miss anything, and moves to
    lower gain settings when the power measurement IIR indicates the system does
    not have enough headroom for the 10dB of PAPR 802.11 allows for.
    
    The AGC threshold values overlap a little bit between the high, mid, and low
    gain settings. This is done on purpose to add hysteresis to the core.
    
    It is important to note that the AGC does not instantaneously go from high to
    low gain, and it will stop at the middle gain setting, wait for the group delays
    of the analog RF components to expire, and collect a new IIR reading before
    deciding it has settled or if it should go on to the low gain stage.
    
    The power ranges and subsequent gain settings are as follows:
    
    High gain: -82dBm - -52dBm
        lna_gain     =>   6 dB (Max)
        rxvga1_gain  =>  30 dB
        rxvga2_gain  =>  15 dB
    
    Mid gain: -52dBm - -30dBm
        lna_gain     =>   3 dB (Mid)
        rxvga1_gain  =>  30 dB
        rxvga2_gain  =>   0 dB
    
    Low gain: -30dBm - -17dBm
        lna_gain     =>   3 dB (Mid)
        rxvga1_gain  =>  12 dB
        rxvga2_gain  =>   0 dB

---
+++ hdl/fpga/ip/nuand/synthesis/lms6002d/vhdl/bladerf_agc.vhd
@@ -0,0 +1,223 @@
+-- Copyright (c) 2017 Nuand LLC
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+library ieee ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
+    use ieee.math_real.all ;
+
+entity bladerf_agc is
+  port (
+    -- 40MHz clock and async asserted, sync deasserted reset
+    clock           :   in  std_logic ;
+    reset           :   in  std_logic ;
+
+    agc_hold_req    :   in  std_logic ;
+
+    gain_inc_req    :  out  std_logic ;
+    gain_dec_req    :  out  std_logic ;
+    gain_rst_req    :  out  std_logic ;
+    gain_ack        :   in  std_logic ;
+    gain_nack       :   in  std_logic ;
+    gain_max        :   in  std_logic ;
+
+    rst_gains       :  out  std_logic ;
+    burst           :  out  std_logic ;
+
+    sample_i        :   in  signed(15 downto 0 ) ;
+    sample_q        :   in  signed(15 downto 0 ) ;
+    sample_valid    :   in  std_logic
+  ) ;
+end entity ;
+
+architecture arch of bladerf_agc is
+
+    type bladerf_sample_t is record
+        i       :   signed(15 downto 0) ;
+        q       :   signed(15 downto 0) ;
+        valid   :   std_logic ;
+    end record ;
+
+    signal iir      :   signed( 31 downto 0 ) ;
+    signal ptemp    :   signed( 31 downto 0 ) ;
+    signal burst_cnt:   signed(  7 downto 0 ) ;
+
+    function run_iir( x : signed( 31 downto 0); y : signed ( 31 downto 0) )
+    return signed
+    is
+        variable amrea : signed(31 downto 0) ;
+    begin
+        amrea := resize( x - shift_right(x, 6) + shift_right(y, 6), 32 );
+        return amrea;
+    end;
+
+    type fsm_t is (IDLE, SETTLE, ATTACK, WAIT_GAIN_ACK, WAIT_GAIN_ACK_1, HOLD) ;
+
+    type state_t is record
+        fsm                 :   fsm_t ;
+        inc_req             :   std_logic ;
+        dec_req             :   std_logic ;
+        rst_req             :   std_logic ;
+        timer               :   unsigned( 10 downto 0 ) ;
+    end record ;
+
+    function NULL_STATE return state_t is
+        variable rv : state_t ;
+    begin
+        rv.fsm          := IDLE ;
+        rv.inc_req      := '0' ;
+        rv.dec_req      := '0' ;
+        rv.rst_req      := '0' ;
+        rv.timer        := ( others => '0' );
+        return rv ;
+    end function ;
+
+    signal current, future  :   state_t := NULL_STATE ;
+    signal sample, sample_out : bladerf_sample_t ;
+begin
+    sample.i <= sample_i;
+    sample.q <= sample_q;
+    sample.valid <= sample_valid;
+
+    gain_inc_req <= current.inc_req ;
+    gain_dec_req <= current.dec_req ;
+    gain_rst_req <= current.rst_req ;
+
+    process( clock )
+    begin
+        if( reset = '1' ) then
+            rst_gains <= '0' ;
+            burst <= '0' ;
+        elsif( rising_edge( clock )) then
+            if( ( gain_max = '1' and iir < 1200 ) or current.fsm = SETTLE ) then
+                rst_gains <= '1' ;
+            else
+                rst_gains <= '0' ;
+            end if ;
+
+            if( gain_max = '0' or iir > 1100 ) then
+                burst <= '1' ;
+                burst_cnt <= to_signed(18, burst_cnt'length) ;
+            else
+                if( ptemp > 1100 ) then
+                    burst <= '1' ;
+                    burst_cnt <= to_signed(6, burst_cnt'length) ;
+                else
+                    if( burst_cnt > 0 ) then
+                        burst <= '1' ;
+                        burst_cnt <= burst_cnt - 1 ;
+                    else
+                        burst <= '0' ;
+                    end if ;
+                end if ;
+            end if ;
+        end if ;
+    end process ;
+
+    process( clock )
+    begin
+        if( reset = '1' ) then
+            iir <= ( others => '0' ) ;
+            ptemp <= (others => '0' ) ;
+        elsif( rising_edge( clock )) then
+            if( current.timer = 18 ) then
+                iir <= ptemp ;
+            elsif( sample.valid = '1') then
+                ptemp <= sample.i * sample.i + sample.q * sample.q ;
+                iir <= run_iir(iir, ptemp) ;
+            end if ;
+        end if ;
+    end process ;
+
+    sync : process(clock, reset)
+    begin
+        if( reset = '1' ) then
+            current <= NULL_STATE ;
+        elsif( rising_edge(clock) ) then
+            current <= future ;
+        end if ;
+    end process ;
+
+    comb : process(all)
+    begin
+        future <= current ;
+        future.inc_req <= '0' ;
+        future.dec_req <= '0' ;
+        future.rst_req <= '0' ;
+
+        case current.fsm is
+            when IDLE =>
+                future.fsm <= SETTLE ;
+
+            when SETTLE =>
+                if( current.timer > 32 ) then -- 50 looks good 70 is overkill
+                    future.timer <= ( others => '0' ) ;
+                    future.fsm <= ATTACK ;
+                else
+                    if( current.timer < 400 ) then
+                        future.timer <= current.timer + 1 ;
+                    end if ;
+                end if ;
+
+            when ATTACK =>
+                if( rst_gains = '1' ) then
+                    future.timer <= ( others => '0' ) ;
+                else
+                    if( current.timer < 400 ) then
+                        future.timer <= current.timer + 1 ;
+                    end if ;
+                end if ;
+
+                if( agc_hold_req = '1' ) then
+                    future.fsm <= HOLD ;
+                else
+                    -- 405000, -48dBm to enter mid, -28dBm to enter low
+                    if( current.timer > 24 and iir > 335000 ) then --335000
+                        future.fsm <= WAIT_GAIN_ACK ;
+                        future.dec_req <= '1' ;
+                    elsif( current.timer < 24 and iir > 485000 ) then --335000
+                        -- I = Q = sqrt(2048*2048/10)=657, IIR = I^2 + Q^2= 845000
+                        future.fsm <= WAIT_GAIN_ACK ;
+                        future.dec_req <= '1' ;
+                    elsif( current.timer > 300 and iir < 1000 ) then
+                    --elsif( iir < 5000 ) then
+                        -- I = Q = 50, IIR = I^2 + Q^2= 5000
+                        future.inc_req <= '1' ;
+                        future.fsm <= WAIT_GAIN_ACK ;
+                    end if ;
+                end if ;
+
+            when WAIT_GAIN_ACK =>
+                future.fsm <= WAIT_GAIN_ACK_1 ;
+            when WAIT_GAIN_ACK_1 =>
+                if( gain_ack = '1' ) then
+                    future.fsm <= SETTLE ;
+                    future.timer <= ( others => '0' ) ;
+                end if ;
+                if( gain_nack = '1' ) then
+                    future.fsm <= ATTACK ;
+                end if ;
+
+            when HOLD =>
+                if( iir < 5000 ) then
+                    -- I = Q = 50, IIR = I^2 + Q^2= 5000
+                    future.rst_req <= '1' ;
+                    future.fsm <= WAIT_GAIN_ACK ;
+                end if ;
+
+        end case ;
+    end process ;
+
+end architecture ;
---
+++ hdl/fpga/ip/nuand/synthesis/lms6002d/vhdl/bladerf_agc_lms_drv.vhd
@@ -0,0 +1,271 @@
+-- Copyright (c) 2017 Nuand LLC
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+library ieee ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
+    use ieee.math_real.all ;
+
+entity bladerf_agc_lms_drv is
+  port (
+    -- 40MHz clock and async asserted, sync deasserted reset
+    clock           :   in  std_logic ;
+    reset           :   in  std_logic ;
+
+    enable          :   in  std_logic ;
+    gain_inc_req    :   in  std_logic ;
+    gain_dec_req    :   in  std_logic ;
+    gain_rst_req    :   in  std_logic ;
+    gain_ack        :  out  std_logic ;
+    gain_nack       :  out  std_logic ;
+
+    gain_high       :  out  std_logic ;
+    gain_mid        :  out  std_logic ;
+    gain_low        :  out  std_logic ;
+        -- Physical Interface
+    sclk            :   out std_logic ;
+    miso            :   in  std_logic ;
+    mosi            :   out std_logic ;
+    cs_n            :   out std_logic
+
+  ) ;
+end entity ;
+
+architecture arch of bladerf_agc_lms_drv is
+    type gain_state_t is ( UNSET_GAIN_STATE, HIGH_GAIN_STATE, MID_GAIN_STATE, LOW_GAIN_STATE ) ;
+    type lna_gain_t is ( LNA_MID_GAIN, LNA_MAX_GAIN ) ;
+    type rxvga1_gain_t is ( RXVGA1_MID_GAIN, RXVGA1_MAX_GAIN ) ;
+    type rxvga2_gain_t is ( RXVGA2_LOW_GAIN, RXVGA2_MID_GAIN ) ;
+
+    type fsm_t is ( INIT, IDLE, SPI_WRITE, WRITE_RXVGA1, WRITE_RXVGA2,
+                    UPDATE_GAINS, SPI_WAIT, SPI_WAIT_1 ) ;
+
+    type gain_t is record
+        lna_gain            :   lna_gain_t ;
+        rxvga1_gain         :   rxvga1_gain_t ;
+        rxvga2_gain         :   rxvga2_gain_t ;
+    end record ;
+
+    -- -82dBm - -52dBm
+    constant HIGH_GAIN : gain_t := (
+            lna_gain     =>  LNA_MAX_GAIN,    --  6 dB (Max)
+            rxvga1_gain  =>  RXVGA1_MAX_GAIN, -- 30 dB
+            rxvga2_gain  =>  RXVGA2_MID_GAIN  -- 15 dB
+        ) ;
+
+    -- -52dBm - -30dBm
+    constant MID_GAIN : gain_t := (
+            lna_gain     =>  LNA_MID_GAIN,    --  3 dB (Mid)
+            rxvga1_gain  =>  RXVGA1_MAX_GAIN, -- 30 dB
+            rxvga2_gain  =>  RXVGA2_LOW_GAIN  --  0 dB
+        ) ;
+
+    -- -30dBm - -17dBm
+    constant LOW_GAIN : gain_t := (
+            lna_gain     =>  LNA_MID_GAIN,    --  3 dB (Mid)
+            rxvga1_gain  =>  RXVGA1_MID_GAIN, -- 12 dB
+            rxvga2_gain  =>  RXVGA2_LOW_GAIN  --  0 dB
+        ) ;
+
+    type state_t is record
+        fsm                 :   fsm_t ;
+        nfsm                :   fsm_t ;
+
+        initializing        :   std_logic ;
+        ack                 :   std_logic ;
+        nack                :   std_logic ;
+        gain_state          :   gain_state_t ;
+        current_gain        :   gain_t ;
+        future_gain         :   gain_t ;
+
+        -- Avalon-MM Interface
+        mm_write            :   std_logic ;
+        mm_addr             :   std_logic_vector(7 downto 0) ;
+        mm_din              :   std_logic_vector(7 downto 0) ;
+    end record ;
+
+    function NULL_STATE return state_t is
+        variable rv : state_t ;
+    begin
+        rv.fsm          := INIT ;
+        rv.nfsm         := INIT ;
+        rv.initializing := '0' ;
+        rv.gain_state   := UNSET_GAIN_STATE ;
+        rv.mm_addr      := ( others => '0' ) ;
+        rv.mm_din       := ( others => '0' ) ;
+        return rv ;
+    end function ;
+
+    signal current, future  :   state_t := NULL_STATE ;
+
+    signal mm_busy          :   std_logic ;
+begin
+
+    gain_high <= '1' when current.gain_state = HIGH_GAIN_STATE else '0' ;
+    gain_mid  <= '1' when current.gain_state = MID_GAIN_STATE else '0' ;
+    gain_low  <= '1' when current.gain_state = LOW_GAIN_STATE else '0' ;
+    gain_ack <= current.ack ; --'1' when current.fsm = UPDATE_GAINS else '0' ;
+    gain_nack <= current.nack ;
+
+    U_spi_controller: entity work.bladerf_spi_controller
+      port map (
+        -- Physical Interface
+        sclk            => sclk,
+        miso            => '0',
+        mosi            => mosi,
+        cs_n            => cs_n,
+
+        -- Avalon-MM Interface
+        mm_clock        => clock,
+        mm_reset        => reset,
+        mm_read         => '0',
+        mm_write        => current.mm_write,
+        mm_addr         => current.mm_addr( 6 downto 0 ),
+        mm_din          => current.mm_din,
+        mm_dout         => open,
+        mm_dout_val     => open,                                  -- Read data valid.
+        mm_busy         => mm_busy
+    );
+
+    sync : process(clock, reset)
+    begin
+        if( reset = '1' ) then
+            current <= NULL_STATE ;
+        elsif( rising_edge(clock) ) then
+            current <= future ;
+        end if ;
+    end process ;
+
+    comb : process(all)
+    begin
+        future <= current ;
+        future.mm_write <= '0' ;
+        future.ack <= '0' ;
+        future.nack <= '0' ;
+
+        case current.fsm is
+            when INIT =>
+                future.nack <= '1' ;
+                if( enable = '1' ) then
+                    future.nack <= '0' ;
+                    future.gain_state <= HIGH_GAIN_STATE ;
+                    future.future_gain <= HIGH_GAIN ;
+                    future.initializing <= '1' ;
+                    future.fsm <= SPI_WRITE ;
+                end if ;
+
+            when IDLE =>
+                if( gain_inc_req = '1' ) then
+                    if( current.gain_state = LOW_GAIN_STATE ) then
+                        future.gain_state <= MID_GAIN_STATE ;
+                        future.future_gain <= MID_GAIN ;
+                        future.fsm <= SPI_WRITE ;
+                    elsif( current.gain_state = MID_GAIN_STATE ) then
+                        future.gain_state <= HIGH_GAIN_STATE ;
+                        future.future_gain <= HIGH_GAIN ;
+                        future.fsm <= SPI_WRITE ;
+                    else
+                        future.nack <= '1' ;
+                        -- we are already as high as can be
+                    end if ;
+                end if ;
+                if( gain_dec_req = '1' ) then
+                    if( current.gain_state = MID_GAIN_STATE ) then
+                        future.gain_state <= LOW_GAIN_STATE ;
+                        future.future_gain <= LOW_GAIN ;
+                        future.fsm <= SPI_WRITE ;
+                    elsif( current.gain_state = HIGH_GAIN_STATE ) then
+                        future.gain_state <= MID_GAIN_STATE ;
+                        future.future_gain <= MID_GAIN ;
+                        future.fsm <= SPI_WRITE ;
+                    else
+                        future.nack <= '1' ;
+                        -- we are already as low as can be
+                    end if ;
+                end if ;
+                if( gain_rst_req = '1' ) then
+                    future.gain_state <= HIGH_GAIN_STATE ;
+                    future.future_gain <= HIGH_GAIN ;
+                end if ;
+                if( enable = '0' ) then
+                    future.fsm <= INIT ;
+                end if ;
+
+            when SPI_WRITE =>
+                if( current.current_gain.lna_gain /= current.future_gain.lna_gain or
+                      current.initializing = '1' ) then
+                    future.mm_addr <= x"75" ;
+                    if (current.future_gain.lna_gain = LNA_MAX_GAIN ) then
+                        future.mm_din <= x"E0"; -- -- x"D0"; --x"E0" ;
+                    elsif (current.future_gain.lna_gain = LNA_MID_GAIN ) then
+                        future.mm_din <= x"A0"; -- x"90"; -- x"A0" ;
+                    end if ;
+                    future.mm_write <= '1' ;
+                    future.fsm <= SPI_WAIT ;
+                    future.nfsm <= WRITE_RXVGA1 ;
+                else
+                    future.fsm <= WRITE_RXVGA1 ;
+                end if ;
+
+            when WRITE_RXVGA1 =>
+                if( current.current_gain.rxvga1_gain /= current.future_gain.rxvga1_gain or
+                      current.initializing = '1' ) then
+                    future.mm_addr <= x"76" ;
+                    if (current.future_gain.rxvga1_gain = RXVGA1_MAX_GAIN ) then
+                        future.mm_din <= x"78" ;
+                    elsif (current.future_gain.rxvga1_gain = RXVGA1_MID_GAIN ) then
+                        future.mm_din <= x"46" ;
+                    end if ;
+                    future.mm_write <= '1' ;
+                    future.fsm <= SPI_WAIT ;
+                    future.nfsm <= WRITE_RXVGA2 ;
+                else
+                    future.fsm <= WRITE_RXVGA2 ;
+                end if ;
+
+            when WRITE_RXVGA2 =>
+                if( current.current_gain.rxvga2_gain /= current.future_gain.rxvga2_gain or
+                      current.initializing = '1' ) then
+                    future.mm_addr <= x"65" ;
+                    if (current.future_gain.rxvga2_gain = RXVGA2_MID_GAIN ) then
+                        future.mm_din <= x"05" ;
+                    elsif (current.future_gain.rxvga2_gain = RXVGA2_LOW_GAIN ) then
+                        future.mm_din <= x"00" ;
+                    end if ;
+                    future.mm_write <= '1' ;
+                    future.fsm <= SPI_WAIT ;
+                    future.nfsm <= UPDATE_GAINS ;
+                else
+                    future.fsm <= UPDATE_GAINS ;
+                end if ;
+
+            when UPDATE_GAINS =>
+                future.ack <= '1' ;
+                future.initializing <= '0' ;
+                future.current_gain <= current.future_gain ;
+                future.fsm <= IDLE ;
+
+            when SPI_WAIT =>
+                future.fsm <= SPI_WAIT_1 ;
+
+            when SPI_WAIT_1 =>
+                if( mm_busy = '0' ) then
+                    future.fsm <= current.nfsm ;
+                end if ;
+
+        end case ;
+    end process ;
+
+end architecture ;
