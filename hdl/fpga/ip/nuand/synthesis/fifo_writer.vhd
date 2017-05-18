de478532a44a9d6ff64a997ba6d41ac1ef8daa91
Brian Glod <brian.glod@nuand.com>
2017-05-18 16:10:51 -0400

    ip: nuand: fifo_writer HDL improvements
    Improved the readability of the fifo_writer.vhd.
    Reduced combinatorial logic, added state machinery for pipelining.
    Seems to have helped reduce the build time slightly (by ~15s or so).

--- hdl/fpga/ip/nuand/synthesis/fifo_writer.vhd
+++ hdl/fpga/ip/nuand/synthesis/fifo_writer.vhd
@@ -1,6 +1,26 @@
-library ieee ;
-    use ieee.std_logic_1164.all ;
-    use ieee.numeric_std.all ;
+-- Copyright (c) 2017 Nuand LLC
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
 
 entity fifo_writer is
   generic (
@@ -8,181 +28,329 @@ entity fifo_writer is
     META_USEDW_WIDTH    :   natural := 5
   );
   port (
-    clock               :   in      std_logic ;
-    reset               :   in      std_logic ;
-    enable              :   in      std_logic ;
+    clock               :   in      std_logic;
+    reset               :   in      std_logic;
+    enable              :   in      std_logic;
 
-    usb_speed           :   in      std_logic ;
-    meta_en             :   in      std_logic ;
+    usb_speed           :   in      std_logic;
+    meta_en             :   in      std_logic;
     timestamp           :   in      unsigned(63 downto 0);
 
-    in_i                :   in      signed(15 downto 0) ;
-    in_q                :   in      signed(15 downto 0) ;
-    in_valid            :   in      std_logic ;
+    in_i                :   in      signed(15 downto 0);
+    in_q                :   in      signed(15 downto 0);
+    in_valid            :   in      std_logic;
 
     fifo_usedw          :   in      std_logic_vector(USEDW_WIDTH-1 downto 0) ;
-    fifo_clear          :   buffer  std_logic ;
-    fifo_write          :   buffer  std_logic ;
-    fifo_full           :   in      std_logic ;
-    fifo_data           :   out     std_logic_vector(31 downto 0) ;
+    fifo_clear          :   buffer  std_logic;
+    fifo_write          :   buffer  std_logic := '0';
+    fifo_full           :   in      std_logic;
+    fifo_data           :   out     std_logic_vector(31 downto 0) := (others => '0');
 
     meta_fifo_full      :   in     std_logic;
     meta_fifo_usedw     :   in     std_logic_vector(META_USEDW_WIDTH-1 downto 0);
-    meta_fifo_data      :   out    std_logic_vector(127 downto 0);
-    meta_fifo_write     :   out    std_logic;
+    meta_fifo_data      :   out    std_logic_vector(127 downto 0) := (others => '0');
+    meta_fifo_write     :   out    std_logic := '0';
 
-    overflow_led        :   buffer  std_logic ;
-    overflow_count      :   buffer  unsigned(63 downto 0) ;
+    overflow_led        :   buffer  std_logic;
+    overflow_count      :   buffer  unsigned(63 downto 0);
     overflow_duration   :   in      unsigned(15 downto 0)
-  ) ;
-end entity ;
+  );
+end entity;
 
 architecture simple of fifo_writer is
 
-    signal buf_enough    : std_logic;
-    signal dma_buf_sz    : signed(12 downto 0);
-    signal dma_downcount : signed(12 downto 0);
-    signal overflow_recovering  :   std_logic ;
-    signal overflow_detected    :   std_logic ;
-    signal meta_written : std_logic ;
-    signal meta_written_reg : std_logic ;
-    signal fifo_write_ready : std_logic;
+    constant DMA_BUF_SIZE_SS   : natural   := 1015;
+    constant DMA_BUF_SIZE_HS   : natural   := 503;
+
+    signal dma_buf_size        : natural range DMA_BUF_SIZE_HS to DMA_BUF_SIZE_SS := DMA_BUF_SIZE_SS;
+
+    signal fifo_enough         : boolean   := false;
+    signal overflow_detected   : std_logic := '0';
+
+    type meta_state_t is (
+        IDLE,
+        META_WRITE,
+        META_DOWNCOUNT
+    );
+
+    type meta_fsm_t is record
+        state           : meta_state_t;
+        dma_downcount   : natural range 0 to DMA_BUF_SIZE_SS;
+        meta_write      : std_logic;
+        meta_data       : std_logic_vector(meta_fifo_data'range);
+        meta_written    : std_logic;
+    end record;
+
+    constant META_FSM_RESET_VALUE : meta_fsm_t := (
+        state           => IDLE,
+        dma_downcount   => 0,
+        meta_write      => '0',
+        meta_data       => (others => '-'),
+        meta_written    => '0'
+    );
+
+    signal meta_current : meta_fsm_t := META_FSM_RESET_VALUE;
+    signal meta_future  : meta_fsm_t := META_FSM_RESET_VALUE;
+
+    type fifo_state_t is (
+        CLEAR,
+        WRITE_SAMPLES,
+        HOLDOFF
+    );
+
+    type fifo_fsm_t is record
+        state           : fifo_state_t;
+        fifo_clear      : std_logic;
+        fifo_write      : std_logic;
+        fifo_data       : std_logic_vector(fifo_data'range);
+    end record;
+
+    constant FIFO_FSM_RESET_VALUE : fifo_fsm_t := (
+        state           => CLEAR,
+        fifo_clear      => '1',
+        fifo_write      => '0',
+        fifo_data       => (others => '-')
+    );
+
+    signal fifo_current : fifo_fsm_t := FIFO_FSM_RESET_VALUE;
+    signal fifo_future  : fifo_fsm_t := FIFO_FSM_RESET_VALUE;
 
 begin
 
-    dma_buf_sz <= to_signed(1015, dma_buf_sz'length) when usb_speed = '0' else to_signed(503, dma_buf_sz'length);
-    process( clock, reset)
-        variable room_in_fifo : std_logic := '0';
+    -- Determine the DMA buffer size based on USB speed
+    calc_buf_size : process( clock, reset )
     begin
-        if( reset = '1') then
-            dma_downcount <= (others =>'0') ;
-            meta_written  <= '0' ;
-            room_in_fifo  := '0';
-        elsif( rising_edge( clock ) ) then
-            if (enable = '1' and meta_en = '1') then
-                if( dma_downcount > 0 ) then
-                    dma_downcount <= dma_downcount - 1 ;
-                elsif ( room_in_fifo = '1' ) then
-                    -- Guaranteed to be done downcounting here .. so only reset the downcount
-                    -- if we are able to store that amount of samples in the downstream FIFO
-                    if( meta_written = '1' or in_valid = '1' ) then
-                        dma_downcount <= dma_buf_sz;
-                        meta_written <= '1' ;
-                    end if ;
-                else
-                    meta_written <= '0' ;
+        if( reset = '1' ) then
+            dma_buf_size <= DMA_BUF_SIZE_SS;
+        elsif( rising_edge(clock) ) then
+            if( usb_speed = '0' ) then
+                dma_buf_size <= DMA_BUF_SIZE_SS;
+            else
+                dma_buf_size <= DMA_BUF_SIZE_HS;
+            end if;
+        end if;
+    end process;
+
+    -- Calculate whether there's enough room in the destination FIFO
+    calc_fifo_free : process( clock, reset )
+        constant FIFO_MAX    : natural := 2**fifo_usedw'length;
+        variable fifo_used_v : unsigned(fifo_usedw'length downto 0) := (others => '0');
+    begin
+        if( reset = '1' ) then
+            fifo_enough <= false;
+            fifo_used_v := (others => '0');
+        elsif( rising_edge(clock) ) then
+            fifo_used_v := unsigned(fifo_full & fifo_usedw);
+            if( (FIFO_MAX - fifo_used_v) > dma_buf_size ) then
+                fifo_enough <= true;
+            else
+                fifo_enough <= false;
+            end if;
+        end if;
+    end process;
+
+
+    -- ------------------------------------------------------------------------
+    -- META FIFO WRITER
+    -- ------------------------------------------------------------------------
+
+    -- Meta FIFO synchronous process
+    meta_fsm_sync : process( clock, reset )
+    begin
+        if( reset = '1' ) then
+            meta_current <= META_FSM_RESET_VALUE;
+        elsif( rising_edge(clock) ) then
+            meta_current <= meta_future;
+        end if;
+    end process;
+
+    -- Meta FIFO combinatorial process
+    meta_fsm_comb : process( all )
+    begin
+
+        meta_future            <= meta_current;
+
+        meta_future.meta_write <= '0';
+        meta_future.meta_data  <= x"FFFFFFFF" & std_logic_vector(timestamp) & x"12344321";
+
+        case meta_current.state is
+            when IDLE =>
+
+                meta_future.dma_downcount <= dma_buf_size;
+
+                if( fifo_enough ) then
+                    meta_future.state  <= META_WRITE;
                 end if;
-            else
-                dma_downcount <= (others =>'0') ;
-                meta_written <= '0' ;
-            end if;
 
-            if( to_signed(2**fifo_usedw'length,fifo_usedw'length+2) - (signed('0'&fifo_full&fifo_usedw)) > dma_buf_sz ) then
-                room_in_fifo := '1';
-            else
-                room_in_fifo := '0';
-            end if;
+            when META_WRITE =>
+
+                if( (meta_fifo_full = '0') and (in_valid = '1') ) then
+                    meta_future.meta_write   <= '1';
+                    meta_future.meta_written <= '1';
+                    meta_future.state        <= META_DOWNCOUNT;
+                end if;
+
+            when META_DOWNCOUNT =>
+
+                if( meta_current.dma_downcount = 2 ) then
+                    -- Look for 2 because of the 2 cycles passing
+                    -- through IDLE and META_WRITE after this.
+                    meta_future.state <= IDLE;
+                else
+                    meta_future.dma_downcount <= meta_current.dma_downcount - 1;
+                end if;
+
+            when others =>
+
+                meta_future.state <= IDLE;
+
+        end case;
+
+        -- Abort?
+        if( (enable = '0') or (meta_en = '0') ) then
+            meta_future.state         <= IDLE;
+            meta_future.meta_written  <= '0';
+        end if;
+
+        -- Output assignments
+        meta_fifo_write <= meta_current.meta_write;
+        meta_fifo_data  <= meta_current.meta_data;
+
+    end process;
+
+
+    -- ------------------------------------------------------------------------
+    -- SAMPLE FIFO WRITER
+    -- ------------------------------------------------------------------------
+
+    -- Sample FIFO synchronous process
+    fifo_fsm_sync : process( clock, reset )
+    begin
+        if( reset = '1' ) then
+            fifo_current <= FIFO_FSM_RESET_VALUE;
+        elsif( rising_edge(clock) ) then
+            fifo_current <= fifo_future;
         end if;
     end process;
 
-    buf_enough <= '1' when ( dma_downcount = (dma_buf_sz)) else '0';
-
-    meta_fifo_write <= '1' when (enable = '1' and meta_en = '1' and buf_enough = '1') else '0';
-    meta_fifo_data <= x"FFFFFFFF" & std_logic_vector(timestamp) & x"12344321";
-
-    meta_written_reg <= '0' when reset = '1' else meta_written when rising_edge(clock) ;
-
-    -- Simple concatenation of samples
-    fifo_data   <= std_logic_vector(in_q & in_i) ;
-    fifo_write  <= in_valid when fifo_write_ready = '1' else '0' ;
-
-    write_ready : process( clock, reset )
+    -- Sample FIFO combinatorial process
+    fifo_fsm_comb : process( all )
     begin
-        if( reset = '1' ) then
-            fifo_write_ready <= '0';
-        elsif( rising_edge(clock) ) then
-            if overflow_recovering = '0' and fifo_full = '0' and (meta_en = '0' or meta_written_reg = '1') then
-                fifo_write_ready <= '1';
-            else
-                fifo_write_ready <= '0';
-            end if;
+
+        fifo_future            <= fifo_current;
+
+        fifo_future.fifo_clear <= '0';
+        fifo_future.fifo_write <= '0';
+        fifo_future.fifo_data  <= std_logic_vector(in_q & in_i);
+
+        case fifo_current.state is
+
+            when CLEAR =>
+
+                if( enable = '1' ) then
+                    fifo_future.fifo_clear <= '0';
+                    fifo_future.state      <= WRITE_SAMPLES;
+                else
+                    fifo_future.fifo_clear <= '1';
+                end if;
+
+            when WRITE_SAMPLES =>
+
+                if( ((meta_current.meta_written = '1') or (meta_en = '0')) ) then
+                    fifo_future.fifo_write <= in_valid;
+                else
+                    fifo_future.fifo_write <= '0';
+                end if;
+
+                if( fifo_full = '1' ) then
+                    fifo_future.fifo_write <= '0';
+                    fifo_future.state      <= HOLDOFF;
+                end if;
+
+            when HOLDOFF =>
+
+                if( fifo_enough ) then
+                    fifo_future.state <= WRITE_SAMPLES;
+                end if;
+
+            when others =>
+
+                fifo_future.state <= CLEAR;
+
+        end case;
+
+        -- Abort?
+        if( enable = '0' ) then
+            fifo_future.fifo_clear <= '1';
+            fifo_future.fifo_write <= '0';
+            fifo_future.state      <= CLEAR;
         end if;
+
+        -- Output assignments
+        fifo_clear <= fifo_current.fifo_clear;
+        fifo_write <= fifo_current.fifo_write;
+        fifo_data  <= fifo_current.fifo_data;
+
     end process;
 
-    -- Clear out the contents when RX is disabled
-    clear_fifo : process( clock, reset )
-    begin
-        if( reset = '1' ) then
-            fifo_clear <= '1' ;
-        elsif( rising_edge(clock) ) then
-            fifo_clear <= '0' ;
-            if( enable = '0' ) then
-                fifo_clear <= '1' ;
-            end if ;
-        end if ;
-    end process ;
+
+    -- ------------------------------------------------------------------------
+    -- OVERFLOW
+    -- ------------------------------------------------------------------------
 
     -- Overflow detection
     detect_overflows : process( clock, reset )
     begin
         if( reset = '1' ) then
-            overflow_detected <= '0' ;
+            overflow_detected <= '0';
         elsif( rising_edge( clock ) ) then
-            overflow_detected <= '0' ;
-            if( enable = '1' and in_valid = '1' and fifo_full = '1' and fifo_clear = '0' ) then
-                overflow_detected <= '1' ;
-            end if ;
-        end if ;
-    end process ;
+            overflow_detected <= '0';
+            if( enable = '1' and in_valid = '1' and
+                fifo_full = '1' and fifo_current.fifo_clear = '0' ) then
+                overflow_detected <= '1';
+            end if;
+        end if;
+    end process;
 
     -- Count the number of times we overflow, but only if they are discontinuous
     -- meaning we have an overflow condition, a non-overflow condition, then
     -- another overflow condition counts as 2 overflows, but an overflow condition
     -- followed by N overflow conditions counts as a single overflow condition.
     count_overflows : process( clock, reset )
-        variable prev_overflow : std_logic := '0' ;
+        variable prev_overflow : std_logic := '0';
     begin
         if( reset = '1' ) then
-            prev_overflow := '0' ;
-            overflow_count <= (others =>'0') ;
-            overflow_recovering <= '0' ;
+            prev_overflow := '0';
+            overflow_count <= (others =>'0');
         elsif( rising_edge( clock ) ) then
-            overflow_recovering <= '0' ;
             if( prev_overflow = '0' and overflow_detected = '1' ) then
-                overflow_count <= overflow_count + 1 ;
-                overflow_recovering <= '1' ;
-            elsif( overflow_recovering = '1' ) then
-                if( fifo_full = '0' ) then
-                    overflow_recovering <= '0' ;
-                end if ;
-            end if ;
-            prev_overflow := overflow_detected ;
-        end if ;
-    end process ;
+                overflow_count <= overflow_count + 1;
+            end if;
+            prev_overflow := overflow_detected;
+        end if;
+    end process;
 
     -- Active high assertion for overflow_duration when the overflow
     -- condition has been detected.  The LED will stay asserted
     -- if multiple overflows have occurred
     blink_overflow_led : process( clock, reset )
-        variable downcount : natural range 0 to 2**overflow_duration'length-1 ;
+        variable downcount : natural range 0 to 2**overflow_duration'length-1;
     begin
         if( reset = '1' ) then
-            downcount := 0 ;
-            overflow_led <= '0' ;
+            downcount := 0;
+            overflow_led <= '0';
         elsif( rising_edge(clock) ) then
             -- Default to not being asserted
-            overflow_led <= '0' ;
+            overflow_led <= '0';
 
             -- Countdown so we can see what happened
             if( overflow_detected = '1' ) then
-                downcount := to_integer(overflow_duration) ;
-            elsif( downcount > 0 ) then
-                downcount := downcount - 1 ;
-                overflow_led <= '1' ;
-            end if ;
+                downcount := to_integer(overflow_duration);
+            elsif( downcount /= 0 ) then
+                downcount := downcount - 1;
+                overflow_led <= '1';
+            end if;
+        end if;
+    end process;
 
-        end if ;
-    end process ;
-
-end architecture ;
-
+end architecture;
