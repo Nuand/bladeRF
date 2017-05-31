05d81a56be62181e8b3a30f33b13228d13390d61
Ryan Tucker <ryan.tucker@nuand.com>
2017-05-31 12:39:24 -0400

    hdl: adi ip: update ad_dds.v to head of vendor dev branch
    ad_dds.v is from commit 84b2ad51e28f59a28e8f1443615975f0f49cb9dd
    
    This adds support for a DISABLE parameter, which can be used to
    bypass the DDS subsystem to save resources.

--- hdl/fpga/ip/analogdevicesinc/hdl/library/common/ad_dds.v
+++ hdl/fpga/ip/analogdevicesinc/hdl/library/common/ad_dds.v
@@ -1,96 +1,90 @@
 // ***************************************************************************
 // ***************************************************************************
-// Copyright 2011(c) Analog Devices, Inc.
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
 //
-// All rights reserved.
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
 //
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//     - Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     - Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in
-//       the documentation and/or other materials provided with the
-//       distribution.
-//     - Neither the name of Analog Devices, Inc. nor the names of its
-//       contributors may be used to endorse or promote products derived
-//       from this software without specific prior written permission.
-//     - The use of this software may or may not infringe the patent rights
-//       of one or more patent holders.  This license does not release you
-//       from the requirement that you obtain separate licenses from these
-//       patent holders to use this software.
-//     - Use of the software either in source or binary form, must be run
-//       on or directly connected to an Analog Devices Inc. component.
+// The user should read each of these license terms, and understand the
+// freedoms and responsabilities that he or she has by using this source/core.
 //
-// THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-// INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
-// PARTICULAR PURPOSE ARE DISCLAIMED.
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
 //
-// IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
-// RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// ***************************************************************************
-// ***************************************************************************
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
 // ***************************************************************************
 // ***************************************************************************
 
 `timescale 1ns/100ps
 
-module ad_dds (
+module ad_dds #(
+
+  // data path disable
+
+  parameter   DISABLE = 0) (
 
   // interface
 
-  clk,
-  dds_format,
-  dds_phase_0,
-  dds_scale_0,
-  dds_phase_1,
-  dds_scale_1,
-  dds_data);
-
-  // interface
-
-  input           clk;
-  input           dds_format;
-  input   [15:0]  dds_phase_0;
-  input   [15:0]  dds_scale_0;
-  input   [15:0]  dds_phase_1;
-  input   [15:0]  dds_scale_1;
-  output  [15:0]  dds_data;
+  input           clk,
+  input           dds_format,
+  input   [15:0]  dds_phase_0,
+  input   [15:0]  dds_scale_0,
+  input   [15:0]  dds_phase_1,
+  input   [15:0]  dds_scale_1,
+  output  [15:0]  dds_data);
 
   // internal registers
 
   reg     [15:0]  dds_data_int = 'd0;
-  reg     [15:0]  dds_data = 'd0;
+  reg     [15:0]  dds_data_out = 'd0;
+  reg     [15:0]  dds_scale_0_d = 'd0;
+  reg     [15:0]  dds_scale_1_d = 'd0;
 
-  reg     [15:0]  dds_scale_0_r = 'd0;
-  reg     [15:0]  dds_scale_1_r = 'd0;
   // internal signals
 
   wire    [15:0]  dds_data_0_s;
   wire    [15:0]  dds_data_1_s;
 
+  // disable
+
+  assign dds_data = (DISABLE == 1) ? 16'd0 : dds_data_out;
+
   // dds channel output
 
   always @(posedge clk) begin
     dds_data_int <= dds_data_0_s + dds_data_1_s;
-    dds_data[15:15] <= dds_data_int[15] ^ dds_format;
-    dds_data[14: 0] <= dds_data_int[14:0];
+    dds_data_out[15:15] <= dds_data_int[15] ^ dds_format;
+    dds_data_out[14: 0] <= dds_data_int[14:0];
   end
 
   always @(posedge clk) begin
-    dds_scale_0_r <= dds_scale_0;
-    dds_scale_1_r <= dds_scale_1;
+    dds_scale_0_d <= dds_scale_0;
+    dds_scale_1_d <= dds_scale_1;
   end
   // dds-1
 
   ad_dds_1 i_dds_1_0 (
     .clk (clk),
     .angle (dds_phase_0),
-    .scale (dds_scale_0_r),
+    .scale (dds_scale_0_d),
     .dds_data (dds_data_0_s));
 
   // dds-2
@@ -98,7 +92,7 @@ module ad_dds (
   ad_dds_1 i_dds_1_1 (
     .clk (clk),
     .angle (dds_phase_1),
-    .scale (dds_scale_1_r),
+    .scale (dds_scale_1_d),
     .dds_data (dds_data_1_s));
 
 endmodule
