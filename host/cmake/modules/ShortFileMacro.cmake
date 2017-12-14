c13b234c19d9b6f477740f120ce199abb49939a2
Rey Tucker <rey.tucker@nuand.com>
2017-12-14 14:06:59 -0500

    host: cmake: ShortFileMacro additional shortening
    We can leave off host/libraries/ and host/utilities/ from pathnames and
    not lose any uniqueness.

--- host/cmake/modules/ShortFileMacro.cmake
+++ host/cmake/modules/ShortFileMacro.cmake
@@ -24,6 +24,13 @@ get_filename_component(BASEPATH "${PROJECT_ROOT_DIR}" REALPATH)
 foreach(FILE ${SRC_TO_SHORTEN})
     get_filename_component(FULL_FILE_NAME ${FILE} REALPATH)
     string(REPLACE "${BASEPATH}/" "" FILE_NAME ${FULL_FILE_NAME})
+
+    # We can trim off a few remaining filename chunks without ambiguity
+    string(REGEX REPLACE "^host/libraries/" "" FILE_NAME ${FILE_NAME})
+    string(REGEX REPLACE "^host/utilities/" "" FILE_NAME ${FILE_NAME})
+
     set_source_files_properties(${FILE} PROPERTIES
                                 COMPILE_FLAGS -DSHORT_FILE_=\"${FILE_NAME}\")
+
+    #message(STATUS "Shortening: ${FULL_FILE_NAME} -> ${FILE_NAME}")
 endforeach()
