31c1a52007f37d76dd86bae7f8f0ad26490ad882
Rey Tucker <rey.tucker@nuand.com>
2017-12-15 18:38:24 -0500

    host: bladeRF-cli: fixup merge issue with AGC commands

--- host/utilities/bladeRF-cli/src/cmd/printset.c
+++ host/utilities/bladeRF-cli/src/cmd/printset.c
@@ -1144,55 +1144,6 @@ static void _print_gpio_bladerf2(uint32_t val)
     printf("    %-20s0x%08x\n", "XB Mode:", xb_mode);
 }
 
-int print_agc(struct cli_state *state, int argc, char **argv)
-{
-    int rv = CLI_RET_OK, status;
-    bladerf_gain_mode mode;
-
-    status = bladerf_get_gain_mode( state->dev, BLADERF_MODULE_RX, &mode );
-    if (status < 0) {
-        state->last_lib_error = status;
-        rv = CLI_RET_LIBBLADERF;
-    } else {
-        printf( "   AGC: %-10s\n",
-                mode == BLADERF_GAIN_MANUAL ? "Disabled" : "Enabled" );
-    }
-    return rv;
-}
-
-int set_agc(struct cli_state *state, int argc, char **argv)
-{
-    int rv = CLI_RET_OK, status;
-    bladerf_gain_mode mode;
-
-    if (argc < 3) {
-        printf( "Usage: set agc <on|off>\n\n" );
-        rv = CLI_RET_NARGS;
-    }
-
-    if (!strcmp(argv[2], "on")) {
-       mode = BLADERF_GAIN_AUTOMATIC;
-    } else if (!strcmp(argv[2], "off")) {
-       mode = BLADERF_GAIN_MANUAL;
-    } else {
-        cli_err_nnl(state, argv[0], "Invalid AGC value (%s)\n", argv[2]);
-        rv = CLI_RET_INVPARAM;
-    }
-
-    if (rv == CLI_RET_OK) {
-        status = bladerf_set_gain_mode( state->dev, BLADERF_MODULE_RX, mode );
-        if (status < 0) {
-            state->last_lib_error = status;
-            rv = CLI_RET_LIBBLADERF;
-        } else {
-            printf( "   AGC: %-10s\n",
-                    mode == BLADERF_GAIN_MANUAL ? "Disabled" : "Enabled" );
-        }
-    }
-
-    return rv;
-}
-
 int print_gpio(struct cli_state *state, int argc, char **argv)
 {
     int rv   = CLI_RET_OK;
