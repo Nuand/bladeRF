655857eecf27580b35cf57e2a3287094b6e4dede
Rey Tucker <rey.tucker@nuand.com>
2018-09-04 12:36:44 -0400

    bladeRF-cli: update copyright date on editline.c

--- host/utilities/bladeRF-cli/src/input/editline.c
+++ host/utilities/bladeRF-cli/src/input/editline.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the bladeRF project
  *
- * Copyright (C) 2013 Nuand LLC
+ * Copyright (C) 2018 Nuand LLC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
