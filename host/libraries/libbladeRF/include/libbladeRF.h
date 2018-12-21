57b6abdc35e034b1b64e121271a9245fd0da3d70
Rey Tucker <rey.tucker@nuand.com>
2018-12-21 16:12:50 -0500

    libbladeRF v2.2.0

--- host/libraries/libbladeRF/include/libbladeRF.h
+++ host/libraries/libbladeRF/include/libbladeRF.h
@@ -51,7 +51,7 @@
  *
  *  https://github.com/Nuand/bladeRF/blob/master/doc/development/versioning.md
  */
-#define LIBBLADERF_API_VERSION (0x02010000)
+#define LIBBLADERF_API_VERSION (0x02020000)
 
 #ifdef __cplusplus
 extern "C" {
