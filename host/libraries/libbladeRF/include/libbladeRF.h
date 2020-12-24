82afb15adb7be91c878069bb65fc1a077dd80e07
Robert Ghilduta <robert.ghilduta@nuand.com>
2020-12-23 20:30:23 -0800

    libbladeRF: bump to version v2.4.0

--- host/libraries/libbladeRF/include/libbladeRF.h
+++ host/libraries/libbladeRF/include/libbladeRF.h
@@ -51,7 +51,7 @@
  *
  *  https://github.com/Nuand/bladeRF/blob/master/doc/development/versioning.md
  */
-#define LIBBLADERF_API_VERSION (0x02020100)
+#define LIBBLADERF_API_VERSION (0x02040000)
 
 #ifdef __cplusplus
 extern "C" {
