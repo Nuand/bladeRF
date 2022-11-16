da14b08f5c23f79f78179cd2dcc222eade6e615f
Ryan Thompson <ryanrthompson10@gmail.com>
2022-11-15 18:34:29 -0600

    libbladeRF: OVERSAMPLE and DEFAULT sample rate guards implemented
    Setting the sample rate will now check with the currently enabled 
    feature. It tests if the sample rate is possible like before
    and then checks if the currently enabled feature allows it.

--- fpga_common/include/bladerf2_common.h
+++ fpga_common/include/bladerf2_common.h
@@ -514,6 +514,22 @@ static struct bladerf_range const bladerf2_sample_rate_range = {
     FIELD_INIT(.scale,  1),
 };
 
+/* Sample Rate Base Range */
+static struct bladerf_range const bladerf2_sample_rate_range_base = {
+    FIELD_INIT(.min,    520834),
+    FIELD_INIT(.max,    61440000),
+    FIELD_INIT(.step,   2),
+    FIELD_INIT(.scale,  1),
+};
+
+/* Sample Rate Oversample Range */
+static struct bladerf_range const bladerf2_sample_rate_range_oversample = {
+    FIELD_INIT(.min,    6250000),
+    FIELD_INIT(.max,    122880000),
+    FIELD_INIT(.step,   2),
+    FIELD_INIT(.scale,  1),
+};
+
 /* Sample rates requiring a 4x interpolation/decimation */
 static struct bladerf_range const bladerf2_sample_rate_range_4x = {
     FIELD_INIT(.min,    520834),
