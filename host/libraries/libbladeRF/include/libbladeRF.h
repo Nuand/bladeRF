ad9fdff1ca6247871280f357a2e71c429788caa0
Robert Ghilduta <robert.ghilduta@nuand.com>
2023-02-13 00:27:44 -0800

    libbladeRF: bump to version v2.5.0
    Introduces 8bit SC8_Q11 mode, and overclocking mode changes for bladeRF 2.0 micro.
    This commits also fixes several bugs, including a locking bug in libbladeRF.
    Help menus and abbreviation support expanded in bladeRF-cli.

--- host/libraries/libbladeRF/include/libbladeRF.h
+++ host/libraries/libbladeRF/include/libbladeRF.h
@@ -51,7 +51,7 @@
  *
  *  https://github.com/Nuand/bladeRF/blob/master/doc/development/versioning.md
  */
-#define LIBBLADERF_API_VERSION (0x02040100)
+#define LIBBLADERF_API_VERSION (0x02050000)
 
 #ifdef __cplusplus
 extern "C" {
