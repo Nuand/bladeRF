b809f00175966103d0b1c97240e773ab2b55c594
Ryan Thompson <ryanrthompson10@gmail.com>
2023-04-20 15:31:46 -0500

    test: txrx_hwloop: test case init moved to init_app_params() and dev->dev_tx

--- host/libraries/libbladeRF_test/test_timestamps/src/test_txrx_hwloop.c
+++ host/libraries/libbladeRF_test/test_timestamps/src/test_txrx_hwloop.c
@@ -45,23 +45,30 @@ struct test_case {
     unsigned int buf_len;
     unsigned int burst_len;     /* Length of a burst, in samples */
     unsigned int iterations;
+    unsigned int num_zero_samples;
 };
 
 
-static void init_app_params(struct app_params *p) {
+static void init_app_params(struct app_params *p, struct test_case *tc) {
+    memset(tc, 0, sizeof(tc[0]));
+    tc->iterations = 10;
+    tc->burst_len = 100e3;
+    tc->num_zero_samples = 1e3;
+    tc->buf_len = 2048;
+
     memset(p, 0, sizeof(p[0]));
     p->samplerate = 1e6;
     p->prng_seed = 1;
-
     p->num_buffers = 16;
     p->num_xfers = 8;
-    p->buf_size = 64 * 1024;
+    p->buf_size = tc->burst_len + tc->num_zero_samples;
     p->timeout_ms = 10000;
 }
 
 int main(int argc, char *argv[]) {
     struct app_params p;
-    struct bladerf *dev = NULL;
+    struct test_case test;
+    struct bladerf *dev_tx = NULL;
     int status, status_out, status_wait;
     unsigned int samples_left;
     size_t i;
@@ -70,29 +77,22 @@ int main(int argc, char *argv[]) {
     struct timeval start, end;
     double time_passed;
 
-    struct test_case test;
-    test.iterations = 10;
-    test.burst_len = 100e3;
-    unsigned int num_zero_samples = 1e3;
-    test.buf_len = 2048;
+    init_app_params(&p, &test);
 
-    init_app_params(&p);
-    p.buf_size = test.burst_len + num_zero_samples;
-
-    samples = calloc(2 * sizeof(int16_t), test.burst_len + num_zero_samples);
+    samples = calloc(2 * sizeof(int16_t), test.burst_len + test.num_zero_samples);
     if (samples == NULL) {
         perror("malloc");
         return BLADERF_ERR_MEM;
     }
 
-    status = bladerf_open(&dev, p.device_str);
+    status = bladerf_open(&dev_tx, p.device_str);
     if (status != 0) {
         fprintf(stderr, "Failed to open device: %s\n",
                 bladerf_strerror(status));
         goto out;
     }
 
-    status = bladerf_set_sample_rate(dev, BLADERF_MODULE_TX, p.samplerate, NULL);
+    status = bladerf_set_sample_rate(dev_tx, BLADERF_MODULE_TX, p.samplerate, NULL);
     if (status != 0) {
         fprintf(stderr, "Failed to set TX sample rate: %s\n",
                 bladerf_strerror(status));
@@ -106,18 +106,18 @@ int main(int argc, char *argv[]) {
 
     memset(&meta, 0, sizeof(meta));
 
-    status = perform_sync_init(dev, BLADERF_MODULE_TX, test.buf_len, &p);
+    status = perform_sync_init(dev_tx, BLADERF_MODULE_TX, test.buf_len, &p);
     if (status != 0) {
         goto out;
     }
 
-    status = bladerf_set_frequency(dev, BLADERF_MODULE_TX, 900e6);
+    status = bladerf_set_frequency(dev_tx, BLADERF_MODULE_TX, 900e6);
     if (status != 0) {
         fprintf(stderr, "Failed to set TX frequency: %s\n", bladerf_strerror(status));
         goto out;
     }
 
-    status = bladerf_get_timestamp(dev, BLADERF_MODULE_TX, &meta.timestamp);
+    status = bladerf_get_timestamp(dev_tx, BLADERF_MODULE_TX, &meta.timestamp);
     if (status != 0) {
         fprintf(stderr, "Failed to get timestamp: %s\n",
                 bladerf_strerror(status));
@@ -132,7 +132,7 @@ int main(int argc, char *argv[]) {
     gettimeofday(&start, NULL);
     for (i = 0; i < test.iterations && status == 0; i++) {
         meta.flags = BLADERF_META_FLAG_TX_BURST_START;
-        samples_left = test.burst_len + num_zero_samples;
+        samples_left = test.burst_len + test.num_zero_samples;
         buf = samples;
 
 
@@ -147,7 +147,7 @@ int main(int argc, char *argv[]) {
                 meta.flags &= ~BLADERF_META_FLAG_TX_BURST_END;
             }
 
-            status = bladerf_sync_tx(dev, buf, to_send, &meta, 10000); //p->timeout_ms);
+            status = bladerf_sync_tx(dev_tx, buf, to_send, &meta, 10000); //p->timeout_ms);
             if (status != 0) {
                 fprintf(stderr, "TX failed @ iteration (%u) %s\n",
                         (unsigned int )i, bladerf_strerror(status));
@@ -165,7 +165,7 @@ int main(int argc, char *argv[]) {
     fflush(stdout);
 
     /* Wait for samples to be transmitted before shutting down the TX module */
-    status_wait = wait_for_timestamp(dev, BLADERF_MODULE_TX, meta.timestamp, 3000);
+    status_wait = wait_for_timestamp(dev_tx, BLADERF_MODULE_TX, meta.timestamp, 3000);
     if (status_wait != 0) {
         status = first_error(status, status_wait);
         fprintf(stderr, "Failed to wait for TX to finish: %s\n",
@@ -175,7 +175,7 @@ int main(int argc, char *argv[]) {
     time_passed = (end.tv_sec - start.tv_sec) + (end.tv_usec - end.tv_usec) / 1000000.0;
     printf("TX finished in %.4f seconds\n", time_passed);
 out:
-    status_out = bladerf_enable_module(dev, BLADERF_MODULE_TX, false);
+    status_out = bladerf_enable_module(dev_tx, BLADERF_MODULE_TX, false);
     if (status_out != 0) {
         fprintf(stderr, "Failed to disable TX module: %s\n",
                 bladerf_strerror(status));
