429c78f1d8e5371e56d0fbfea7f5c9c4cfb3c05c
Ryan Thompson <ryanrthompson10@gmail.com>
2023-08-02 14:18:14 -0500

    test: fx3_gpif_meta_tb added to regression test

--- host/libraries/libbladeRF_test/test_general/src/main.cxx
+++ host/libraries/libbladeRF_test/test_general/src/main.cxx
@@ -357,6 +357,10 @@ TEST_HDL_VERIFY(fx3_gpif_tb, 16b01, -gENABLE_CHANNEL_0='0' -gENABLE_CHANNEL_1='1
 TEST_HDL_VERIFY(fx3_gpif_tb, 16b10, -gENABLE_CHANNEL_0='1' -gENABLE_CHANNEL_1='0', 1000);
 TEST_HDL_VERIFY(fx3_gpif_tb, 16b11, -gENABLE_CHANNEL_0='1' -gENABLE_CHANNEL_1='1', 1000);
 
+TEST_HDL_VERIFY(fx3_gpif_meta_tb, 16b00, -gENABLE_CHANNEL_0='0' -gENABLE_CHANNEL_1='0', 1000);
+TEST_HDL_VERIFY(fx3_gpif_meta_tb, 16b01, -gENABLE_CHANNEL_0='0' -gENABLE_CHANNEL_1='1', 1000);
+TEST_HDL_VERIFY(fx3_gpif_meta_tb, 16b10, -gENABLE_CHANNEL_0='1' -gENABLE_CHANNEL_1='0', 1000);
+TEST_HDL_VERIFY(fx3_gpif_meta_tb, 16b11, -gENABLE_CHANNEL_0='1' -gENABLE_CHANNEL_1='1', 1000);
 
 TEST_P(verify, fx3_gpif_iq_8bit_tb) {
     std::string bitmode  = std::get<0>(GetParam()) ? "1" : "0";
