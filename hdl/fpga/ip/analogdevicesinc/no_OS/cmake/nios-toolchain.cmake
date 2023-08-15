f046180604ac7f8f9bb0e2d1b7d9ddb2a92878da
Ryan Thompson <ryanrthompson10@gmail.com>
2023-08-15 16:10:58 -0500

    host: CMake minimum version bump to 3.5
    Accommodates the following warning issued by CMake v3.27.1.
    
    CMake Deprecation Warning:
      Compatibility with CMake < 3.5 will be removed from a 
      future version of CMake.

--- hdl/fpga/ip/analogdevicesinc/no_OS/cmake/nios-toolchain.cmake
+++ hdl/fpga/ip/analogdevicesinc/no_OS/cmake/nios-toolchain.cmake
@@ -6,7 +6,7 @@
 #
 #
 ################################################################################
-cmake_minimum_required(VERSION 2.8.12)
+cmake_minimum_required(VERSION 3.5)
 
 if(NOT "$ENV{QUARTUS_ROOTDIR}" STREQUAL "")
     set(QUARTUS_ROOTDIR "$ENV{QUARTUS_ROOTDIR}")
