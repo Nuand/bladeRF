9fc57e6c7feabde04feebd62cc5b8bb83223728c
Robert Ghilduta <robert.ghilduta@nuand.com>
2023-09-19 13:23:25 -0700

    libbladeRF: fix typo in sync_tx

--- host/libraries/libbladeRF/src/streaming/sync.c
+++ host/libraries/libbladeRF/src/streaming/sync.c
@@ -1209,7 +1209,7 @@ int sync_tx(struct bladerf_sync *s,
                                    samples2bytes(s, samples_to_copy));
 
                             s->meta.curr_msg_off += samples_to_copy;
-                            if (s->stream_config.layout == BLADERF_RX_X2)
+                            if (s->stream_config.layout == BLADERF_TX_X2)
                                s->meta.curr_timestamp += samples_to_copy / 2;
                             else
                                s->meta.curr_timestamp += samples_to_copy;
