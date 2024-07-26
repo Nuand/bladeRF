bdd4f39a3dfdf9f4f6ca414e531a6cbffc36b1a2
Ryan Thompson <ryan.thompson@nuand.com>
2024-07-26 17:29:48 -0500

    fx3_firmware: rev libbladeRF, hdl, and fx3 for 2048 sample gpif buffers
    FX3 GPIF buffers set to 2048 samples and number of buffers reduced to
    11 per dma channel. This substantially increases the theoretical USB
    SuperSpeed throughput from roughly 228MB/s to 356MB/s.
    
    $ make test_libbladeRF
    [       OK ] libbladeRF.scheduled_retune (19755 ms)
    [----------] 29 tests from libbladeRF (308802 ms total)
    
    [----------] Global test environment tear-down
    [==========] 29 tests from 1 test suite ran. (308802 ms total)
    [  PASSED  ] 29 tests.

--- host/libraries/libbladeRF/src/board/bladerf1/compatibility.c
+++ host/libraries/libbladeRF/src/board/bladerf1/compatibility.c
@@ -13,6 +13,7 @@
 
 static const struct compat fw_compat[] = {
     /*   Firmware       requires  >=        FPGA */
+    { VERSION(2, 5, 0),                 VERSION(0, 16, 0) },
     { VERSION(2, 4, 0),                 VERSION(0, 6, 0) },
     { VERSION(2, 3, 2),                 VERSION(0, 0, 2) },
     { VERSION(2, 3, 1),                 VERSION(0, 0, 2) },
@@ -35,6 +36,7 @@ const struct version_compat_table bladerf1_fw_compat_table = {fw_compat, ARRAY_S
 
 static const struct compat fpga_compat[] = {
     /*    FPGA          requires >=        Firmware */
+    { VERSION(0, 16, 0),                VERSION(2, 5, 0) },
     { VERSION(0, 15, 3),                VERSION(2, 4, 0) },
     { VERSION(0, 15, 1),                VERSION(2, 4, 0) },
     { VERSION(0, 15, 0),                VERSION(2, 4, 0) },
--- host/libraries/libbladeRF/src/board/bladerf2/compatibility.c
+++ host/libraries/libbladeRF/src/board/bladerf2/compatibility.c
@@ -13,6 +13,7 @@
 
 static const struct compat fw_compat[] = {
     /*   Firmware       requires  >=        FPGA */
+    { VERSION(2, 5, 0),                 VERSION(0, 16, 0) },
     { VERSION(2, 4, 0),                 VERSION(0, 6, 0) },
     { VERSION(2, 3, 2),                 VERSION(0, 6, 0) },
     { VERSION(2, 3, 1),                 VERSION(0, 6, 0) },
@@ -27,6 +28,7 @@ const struct version_compat_table bladerf2_fw_compat_table = {fw_compat, ARRAY_S
 
 static const struct compat fpga_compat[] = {
     /*    FPGA          requires >=        Firmware */
+    { VERSION(0, 16, 0),                VERSION(2, 5, 0) },
     { VERSION(0, 15, 3),                VERSION(2, 4, 0) },
     { VERSION(0, 15, 2),                VERSION(2, 4, 0) },
     { VERSION(0, 15, 1),                VERSION(2, 4, 0) },
