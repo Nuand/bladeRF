5ba9b6d916114ed14347eeb8a5318d1b546d63fc
Ryan Thompson <ryan.thompson@nuand.com>
2024-07-26 17:29:48 -0500

    fx3_firmware: rev libbladeRF, hdl, and fx3 for 2048 sample gpif buffers
    FX3 GPIF buffers set to 2048 samples and number of buffers reduced to
    11 per dma channel. This substantially increases the theoretical USB
    SuperSpeed throughput from roughly 228MB/s to 356MB/s.
    
    $ make test_libbladeRF
    [       OK ] libbladeRF.scheduled_retune (19755 ms)
    [----------] 29 tests from libbladeRF (308802 ms total)
    
    [----------] Global test environment tear-down
    [==========] 29 tests from 1 test suite ran. (308802 ms total)
    [  PASSED  ] 29 tests.

--- host/libraries/libbladeRF/include/libbladeRF.h
+++ host/libraries/libbladeRF/include/libbladeRF.h
@@ -51,7 +51,7 @@
  *
  *  https://github.com/Nuand/bladeRF/blob/master/doc/development/versioning.md
  */
-#define LIBBLADERF_API_VERSION (0x02050100)
+#define LIBBLADERF_API_VERSION (0x02060000)
 
 #ifdef __cplusplus
 extern "C" {
