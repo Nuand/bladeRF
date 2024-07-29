09619ffc6ec1aaafd77deec0570740aa3c0e4502
Ryan Thompson <ryan.thompson@nuand.com>
2024-07-29 14:23:05 -0500

    fx3_firmware: increase DMA buffer sizes for USB throughput bottleneck
    $ make test_hdl
    [----------] Global test environment tear-down
    [==========] 63 tests from 2 test suites ran. (381209 ms total)
    [  PASSED  ] 63 tests.
    [100%] Built target test_hdl

--- host/libraries/libbladeRF/src/backend/usb/usb.h
+++ host/libraries/libbladeRF/src/backend/usb/usb.h
@@ -61,8 +61,8 @@ extern bool bladerf_usb_reset_device_on_open;
 #endif
 
 /* Size of a host<->FPGA message in BYTES */
-#define USB_MSG_SIZE_SS 2048
-#define USB_MSG_SIZE_HS 1024
+#define USB_MSG_SIZE_SS 8192
+#define USB_MSG_SIZE_HS 4096
 
 typedef enum {
     USB_TARGET_DEVICE,
