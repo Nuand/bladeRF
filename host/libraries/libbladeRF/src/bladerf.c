1494dce7e76f0dfadd891f12d93532a07e06f7f9
subrad <bradley@distributedspectrum.com>
2025-04-10 09:45:04 -0400

    only attempt to apply gain corr if dev in MGC mode
    This will help when in AGC sweep mode and it thinks every freq is clamping gain

--- host/libraries/libbladeRF/src/bladerf.c
+++ host/libraries/libbladeRF/src/bladerf.c
@@ -833,8 +833,11 @@ int bladerf_set_frequency(struct bladerf *dev,
     MUTEX_LOCK(&dev->lock);
 
     status = dev->board->set_frequency(dev, ch, frequency);
+    bladerf_gain_mode gain_mode;
 
-    if (dev->gain_tbls[ch].enabled && status == 0) {
+    dev->board->get_gain_mode(dev, ch, &gain_mode);
+
+    if ((dev->gain_tbls[ch].enabled && status == 0) && (gain_mode == BLADERF_GAIN_MGC)) {
         status = apply_gain_correction(dev, ch, frequency);
         if (status != 0) {
             log_error("Failed to set gain correction\n");
